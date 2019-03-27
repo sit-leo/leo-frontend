5bd21e1799504c761f1453ea8637bb4a3150dbf6
Keerati <keerati.jear@gmail.com>
2019-03-27 16:23:26 +0700

    [UPDATE] update to get match id from query

--- pages/matches/match.js
+++ pages/matches/match.js
@@ -5,8 +5,8 @@ import { setMatch, getMatchByMatchId } from '../../store/match';
 import MatchPage from '../../components/matches/match';
 
 class MatchSSR extends React.Component {
-  static async getInitialProps({ store }) {
-    const match = await getMatchByMatchId();
+  static async getInitialProps({ store, query }) {
+    const match = await getMatchByMatchId(query.matchId);
     await store.dispatch(setMatch(match));
     return { match: store.getState().match.match };
   }
