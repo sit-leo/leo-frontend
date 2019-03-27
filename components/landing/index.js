35ae3c138cc5878db832b97e25826b9cd4c8c446
Keerati <keerati.jear@gmail.com>
2019-03-27 15:53:17 +0700

    [UPDATE] update landing page

--- components/landing/index.js
+++ components/landing/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 
-import Button from '../base/Button';
+import env from '../../config/env';
 
-const LandingIndex = ({
-  user, getUserByUsername,
-  username, updateUsername,
-  loading, setLoading,
-}) => (
+const LandingIndex = () => (
   <React.Fragment>
-    <h1>{ username }</h1>
-    <h1>{ user.name }</h1>
-    <h1>{ `${loading}` }</h1>
-    <h1>Landing Page</h1>
-    <input type="text" value={username} onChange={e => updateUsername(e.target.value)} />
-    <Button loading={loading} onClick={() => getUserByUsername(username)} text="Hi Next.js!" />
-    <Button loading={loading} onClick={() => setLoading(!loading)} text="Set Loading" />
+    <h1>Hello LEO !</h1>
+    <h3>{ env.public.type }</h3>
+    <h3>{ env.public.api }</h3>
   </React.Fragment>
 );
 
