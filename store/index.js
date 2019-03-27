ef3bff7c66e7a9472583a855cd77db253893c458
Keerati <keerati.jear@gmail.com>
2019-03-27 16:03:45 +0700

    [ADD] Add match reducer to store

--- store/index.js
+++ store/index.js
@@ -3,14 +3,14 @@ import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import loading from './theme/loading';
 import user from './account/user';
+import match from './match';
 
 const logger = createLogger();
 
 const middleware = [thunk, logger];
 
-export const reducers = combineReducers({ loading, user });
+export const reducers = combineReducers({ match, user });
 
 function initialState() {
   return createStore(reducers, composeWithDevTools(applyMiddleware(...middleware)));
