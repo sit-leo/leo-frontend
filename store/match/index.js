444bd032b7e08d60d19ca31f4dfaac2ba3a037a5
Keerati <keerati.jear@gmail.com>
2019-03-27 16:04:05 +0700

    [ADD] Add match store with get match by id and set match

---
+++ store/match/index.js
@@ -0,0 +1,38 @@
+import Router from 'next/router';
+
+import env from '../../config/env';
+import request from '../../tools/request';
+
+const MATCH_API = env.public.matchingApi;
+
+const initState = {
+  loading: false,
+  match: {
+    id: 0,
+    name: '',
+  },
+};
+
+// Actions
+const SET_LOADING = 'leo/match/set/loading';
+
+const SET_MATCH = 'leo/match/set/match';
+
+// Reducer
+export default function reducer(state = initState, action = {}) {
+  switch (action.type) {
+    case SET_LOADING: return { ...state, loading: action.loading };
+    case SET_MATCH: return { ...state, match: action.match };
+    default: return { ...state };
+  }
+}
+
+// Action Creators
+export function setMatch(match) {
+  return { type: SET_MATCH, match };
+}
+
+export function getMatchByMatchId(matchId) {
+  return request.get(`${MATCH_API}/matches/${matchId}`)
+    .then(({ data: match }) => match);
+}
