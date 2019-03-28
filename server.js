01f7658068050cd8c3023dd3319ff9d126d7c593
Keerati <keerati.jear@gmail.com>
2019-03-28 17:49:10 +0700

    [UPDATE] Update PORT config

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 
 const routes = require('./routes');
 
+const PORT = process.env.PORT || 3000;
+
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-  express().use(handler).listen(3000);
+  express().use(handler).listen(PORT);
 });
