ffb3092662cf386668103e20e21de9aad463c870
Keerati <keerati.jear@gmail.com>
2019-03-29 12:08:33 +0700

    [UPDATE] UPDATE port env

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 3000;
 
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
+  const PORT = process.env.PORT || 3000;
   express().use(handler).listen(PORT);
 });
