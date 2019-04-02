b60113713df54359d7647e8e2188f4eae01eb25b
Keerati <keerati.jear@gmail.com>
2019-04-02 11:47:44 +0700

    [ADD] add colors config

--- config/color.js
+++ config/color.js
@@ -1,6 +1,11 @@
 export default {
-  primary: '#58A4B0',
-  secondary: '#A9BCD0',
-  warning: '#1B1B1E',
-  danger: '#ee4266',
+  primary: '#58a4b0',
+  text: {
+    main: '#373F51',
+    paragraph: '#1b1b1e',
+    outline: '#A9BCD0',
+  },
+  error: '#d96c6c',
+  disabled: '#d8d8d8',
+  background: '#F6F6F6',
 };
