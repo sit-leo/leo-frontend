6104f46b5bb79f6737e2d97ebaaae9be06cd0233
Keerati <keerati.jear@gmail.com>
2019-04-02 13:48:41 +0700

    [ADD] add hover  button colors

--- config/color.js
+++ config/color.js
@@ -1,10 +1,11 @@
 export default {
   primary: '#58a4b0',
+  hover: '#3c9fae',
   text: {
     main: '#373F51',
     paragraph: '#1b1b1e',
-    outline: '#A9BCD0',
   },
+  outline: '#A9BCD0',
   error: '#d96c6c',
   disabled: '#d8d8d8',
   background: '#F6F6F6',
