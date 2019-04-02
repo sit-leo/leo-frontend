54316ddbbd16d8823d1bc1465955092ae643d6c9
Keerati <keerati.jear@gmail.com>
2019-04-02 14:04:21 +0700

    [UPDATE] update container background

---
+++ components/base/Container.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import colors from '../../config/color';
+
+const Container = styled.div`
+  height: auto;
+  min-height: 100vh;
+  background: ${colors.background};
+`;
+
+export default Container;
--- components/landing/index.js
+++ components/landing/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import env from '../../config/env';
 
+import Container from '../base/Container';
 import Text, { TitlePrimary, Paragraph, TextError } from '../base/Text';
 import Input from '../base/Input';
 
 const LandingIndex = () => (
-  <React.Fragment>
+  <Container>
     <TitlePrimary>Hello LEO !</TitlePrimary>
     <TextError>{ env.public.type }</TextError>
     <br />
@@ -18,7 +19,7 @@ const LandingIndex = () => (
     <Paragraph>Paragraph Text</Paragraph>
     <TextError>Error Text</TextError>
     <Input text="Input" />
-  </React.Fragment>
+  </Container>
 );
 
 export default LandingIndex;
