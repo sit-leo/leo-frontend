7b409b994e29d1a3aae919bad4132090c7530f1a
Keerati <keerati.jear@gmail.com>
2019-04-04 17:04:48 +0700

    [ADD] Add custom grid system

---
+++ components/base/Grid.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import {
+  Container as ContainerDefault,
+  Row as DefaultRow,
+  Col as DefaultCol,
+} from 'reactstrap';
+
+import colors from '../../config/color';
+
+export const Col = ({
+  xs = 12, md, lg, xl, children, ...props
+}) => (
+  <DefaultCol xs={xs} md={md} lg={lg} xl={xl} {...props}>
+    { children }
+  </DefaultCol>
+);
+
+export const Row = DefaultRow;
+
+export const ContainerStyled = styled(ContainerDefault)`
+  height: auto;
+  min-height: 100vh;
+  background: ${colors.background};
+`;
+
+export const ContainerFluid = ({ children }) => (
+  <ContainerStyled fluid>
+    { children }
+  </ContainerStyled>
+);
+
+const Container = ({ children }) => (
+  <ContainerStyled fluid={false}>
+    <Row>
+      { children }
+    </Row>
+  </ContainerStyled>
+);
+
+export default Container;
