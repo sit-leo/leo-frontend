cdb37fd393e46a1943da0c23d7c1785d609eefec
Keerati <keerati.jear@gmail.com>
2019-04-04 17:06:49 +0700

    [ADD] Add Ranking Page Component

--- components/ranking/index.js
+++ components/ranking/index.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const RankingPage = () => (
-  <div>Ranking</div>
+import { ContainerFluid, Row, Col } from '../base/Grid';
+import Hero from '../base/Hero';
+import Card from '../base/Card';
+import { TitleMedium } from '../base/Text';
+
+const Position = ({ position }) => (
+  <div>
+    {
+      position.name
+    }
+  </div>
 );
 
-export default RankingPage;
+const RankingPage = ({ positions }) => (
+  <ContainerFluid>
+    <Row>
+      <Col className="px-0">
+        <Hero text="Ranking Page" />
+      </Col>
+    </Row>
+    <Row className="py-4 px-5">
+      <Col lg={4}>
+        <TitleMedium>Your Ranking</TitleMedium>
+        <Card>
+          Your Ranking
+        </Card>
+      </Col>
+      <Col lg={8}>
+        <TitleMedium>List of Recruiters</TitleMedium>
+        <Card>
+          {
+            positions.map(position => <Position key={position.id} position={position} />)
+          }
+        </Card>
+      </Col>
+    </Row>
+  </ContainerFluid>
+);
+
+const mapStateToProps = state => ({
+  positions: state.match.positions,
+});
+
+export default connect(mapStateToProps)(RankingPage);
