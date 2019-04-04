f52f7a88114549affccc64253d8b2d49ffcf2772
Keerati <keerati.jear@gmail.com>
2019-04-04 10:11:49 +0700

    [ADD] add full of fonts from style guide

--- components/base/Text.js
+++ components/base/Text.js
@@ -2,18 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 
 import colors from '../../config/color';
+import fonts from '../../config/font';
 
 const Text = styled.span`
-  color: ${colors.text.main};
+  font-size: ${fonts.size.paragraph};
+  color: ${colors.text.paragraph};
 `;
 
 export default Text;
 
-export const TitlePrimary = styled.h1`
+export const Title = styled.h1`
+  font-size: ${fonts.size.large};
+  color: ${colors.text.main};
+`;
+
+export const TitlePrimary = styled(Title)`
   color: ${colors.primary};
 `;
 
+export const TitleMedium = styled.h2`
+  font-size: ${fonts.size.medium};
+  color: ${colors.text.main};
+`;
+
+export const TextSmall = styled.h4`
+  font-size: ${fonts.size.small};
+  color: ${colors.text.paragraph};
+  font-weight: normal;
+`;
+
+export const TitleSmall = styled(TextSmall)`
+  font-weight: 600;
+`;
+
 export const Paragraph = styled.p`
+  font-size: ${fonts.size.paragraph};
   color: ${colors.text.paragraph};
 `;
 
