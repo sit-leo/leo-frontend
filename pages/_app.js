d1567cdb8e3b6f05cb28e8c6064acc83a6b73118
Keerati <keerati.jear@gmail.com>
2019-04-05 12:18:00 +0700

    [ADD] add global styled

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Provider } from 'react-redux';
 import App, { Container } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+
 import withReduxStore from '../tools/with-redux-store';
+import colors from '../config/color';
+
+
+const GlobalStyle = createGlobalStyle`
+  div#__next, html, body {
+    background: ${colors.background};
+  }
+`;
 
 export default withReduxStore(
   class MyApp extends App {
@@ -12,6 +22,7 @@ export default withReduxStore(
       const { Component, pageProps, store } = this.props;
       return (
         <Container>
+          <GlobalStyle />
           <Provider store={store}>
             <Component {...pageProps} />
           </Provider>
