80f9232072aa7acf5ac988ce43cb411c27da83c3
Keerati <keerati.jear@gmail.com>
2019-04-05 11:29:23 +0700

    [ADD] add small card example

--- .storybook/stories/base/card.js
+++ .storybook/stories/base/card.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import Card from '../../../components/base/Card';
+import Card, { SmallCard } from '../../../components/base/Card';
 
 storiesOf('Base/Card', module)
   .add('Card Component', () => (
     <Card>Example Card</Card>
+  ))
+  .add('Small Card Component', () => (
+    <SmallCard>Example Small Card</SmallCard>
   ))
