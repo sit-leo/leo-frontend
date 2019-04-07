74a4697a8f2416b1d8572c839bc09e8d246f9e1f
Keerati <keerati.jear@gmail.com>
2019-04-07 19:28:26 +0700

    [ADD] add cookie for server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const next = require('next');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
-
 
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
   const PORT = process.env.PORT || 3000;
-  express().use(handler).listen(PORT);
+  const server = express();
+  server.use(cookieParser());
+  server.use(handler).listen(PORT);
 });
