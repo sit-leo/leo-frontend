eebda49195b97242d4a6ebb36043c504f1f81a2e
Keerati <keerati.jear@gmail.com>
2019-04-07 19:34:24 +0700

    [UPDATE] update page to use server side instance

--- pages/matches/match.js
+++ pages/matches/match.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-import { setMatch, getMatchByMatchId } from '../../store/match';
+import { serverInstance } from '../../tools/request';
+import adapter from '../../store/match/match-adapter';
+
+import { setMatch } from '../../store/match';
 
 import MatchPage from '../../components/matches/match';
 
 class MatchSSR extends React.Component {
-  static async getInitialProps({ store, query }) {
-    const match = await getMatchByMatchId(query.matchId);
+  static async getInitialProps({ store, query, req }) {
+    const matchAdapter = adapter(serverInstance(req.cookies.token));
+    const match = await matchAdapter.getMatchByMatchId(query.matchId);
     await store.dispatch(setMatch(match));
-    return { match: store.getState().match.match };
+    return {};
   }
 
   render() {
-    const { match } = this.props;
-    return <MatchPage match={match} />;
+    return <MatchPage />;
   }
 }
 
-export default MatchSSR;
+export default connect()(MatchSSR);
--- pages/ranking.js
+++ pages/ranking.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { setPositions, getPositionsByMatchId } from '../store/match';
+import { serverInstance } from '../tools/request';
+import adapter from '../store/match/match-adapter';
+
+import { setPositions } from '../store/match';
 
 import RankingPage from '../components/ranking';
 
 class Ranking extends React.Component {
-  static async getInitialProps({ store, query }) {
-    const positions = await getPositionsByMatchId(query.matchId);
+  static async getInitialProps({ store, query, req }) {
+    const matchAdapter = adapter(serverInstance(req.cookies.token));
+    const positions = await matchAdapter.getPositionsByMatchId(query.matchId);
     await store.dispatch(setPositions(positions));
     return {};
   }
@@ -16,6 +20,5 @@ class Ranking extends React.Component {
     return <RankingPage />;
   }
 }
-
 
 export default connect()(Ranking);
