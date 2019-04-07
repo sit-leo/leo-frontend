54200262c1d834841cce7f5961738db638cfbc61
Keerati <keerati.jear@gmail.com>
2019-04-07 19:38:27 +0700

    [UPDATE] update match adapter to available to inject instance

--- store/match/match-adapter.js
+++ store/match/match-adapter.js
@@ -1,16 +1,20 @@
 import Router from 'next/router';
 
 import env from '../../config/env';
-
 import request from '../../tools/request';
 
 const MATCH_API = env.public.matchingApi;
-export function getMatchByMatchId(matchId) {
-  return request.get(`${MATCH_API}/matches/${matchId}`)
-    .then(({ data: match }) => match);
-}
 
-export function getPositionsByMatchId(matchId) {
-  return request.get(`${MATCH_API}/matches/${matchId}/positions`)
-    .then(({ data: positions }) => positions);
-}
+export default (instance) => {
+  const adapter = request(instance);
+  return {
+    getMatchByMatchId(matchId) {
+      return adapter.get(`${MATCH_API}/matches/${matchId}`)
+        .then(({ data: match }) => match);
+    },
+    getPositionsByMatchId(matchId) {
+      return adapter.get(`${MATCH_API}/matches/${matchId}/positions`)
+        .then(({ data: positions }) => positions);
+    },
+  };
+};
