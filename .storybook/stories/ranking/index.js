97059944b5c338d5db960c9c1115133643512a6b
Keerati <keerati.jear@gmail.com>
2019-04-08 17:47:22 +0700

    [UPDATE] update storybook redux

--- .storybook/stories/ranking/index.js
+++ .storybook/stories/ranking/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 import { RankingPage } from '../../../components/ranking';
 
+const mockStore = configureStore();
+const store = mockStore({
+  ui: {
+    isMenuOpened: true,
+  },
+});
+
 storiesOf('Ranking', module)
+  .addDecorator(getStory => <Provider store={store}>{getStory()}</Provider>)
   .add('Ranking Component', () => (
     <RankingPage text="Default" />
   ))
