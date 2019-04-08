13a147730e4bb0a0ec9826924d992e1d189ba0de
Keerati <keerati.jear@gmail.com>
2019-04-08 19:55:07 +0700

    [UPDATE] update to fetch ranking for use matchId

--- pages/ranking.js
+++ pages/ranking.js
@@ -4,14 +4,16 @@ import { connect } from 'react-redux';
 import { serverInstance } from '../tools/request';
 import adapter from '../store/match/match-adapter';
 
-import { setPositions } from '../store/match';
+import { setMatch, setPositions } from '../store/match';
 
 import RankingPage from '../components/ranking';
 
 class Ranking extends React.Component {
   static async getInitialProps({ store, query, req }) {
     const matchAdapter = adapter(serverInstance(req.cookies.token));
+    const match = await matchAdapter.getMatchByMatchId(query.matchId);
     const positions = await matchAdapter.getPositionsByMatchId(query.matchId);
+    await store.dispatch(setMatch(match));
     await store.dispatch(setPositions(positions));
     return {};
   }
