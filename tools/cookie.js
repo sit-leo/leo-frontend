53fd35f557ec52420c3d9a8a55580da7f562d9b3
Keerati <keerati.jear@gmail.com>
2019-04-09 02:05:44 +0700

    [UPDATE] update how to get cookie

--- tools/cookie.js
+++ tools/cookie.js
@@ -2,11 +2,14 @@ import cookie from 'js-cookie';
 
 const COOKIE_TOKEN = 'token';
 
-const inHalfADay = 0.5;
+const getToken = (req) => {
+  if (process.browser) {
+    return cookie.get(COOKIE_TOKEN);
+  }
+  return req && req.cookies && req.cookies.token;
+};
 
-const getToken = () => cookie.get(COOKIE_TOKEN);
-
-const setToken = token => cookie.set(COOKIE_TOKEN, token, { expires: inHalfADay });
+const setToken = (token, expires) => cookie.set(COOKIE_TOKEN, token, { expires: new Date(expires) });
 
 const clearToken = () => cookie.remove(COOKIE_TOKEN);
 
