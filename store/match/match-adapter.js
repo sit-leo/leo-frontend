78eb3be9735efbefb7d224ddd89985eadd71d9af
Keerati <keerati.jear@gmail.com>
2019-04-09 02:05:38 +0700

    [UPDATE] update adapter instance

--- store/match/match-adapter.js
+++ store/match/match-adapter.js
@@ -9,20 +9,17 @@ function redirectToRanking(matchId) {
   return Router.push(`/matches/${matchId}/ranking`);
 }
 
-export default (instance) => {
-  const adapter = request(instance);
-  return {
-    getMatchByMatchId(matchId) {
-      return adapter.get(`${MATCH_API}/matches/${matchId}`)
-        .then(({ data: match }) => match);
-    },
-    getPositionsByMatchId(matchId) {
-      return adapter.get(`${MATCH_API}/matches/${matchId}/positions`)
-        .then(({ data: positions }) => positions);
-    },
-    postApplicantRankingByMatchId(matchId, applicantRanking) {
-      return adapter.post(`${MATCH_API}/matches/${matchId}/ranking`, applicantRanking)
-        .then(({ status }) => status === 200 && redirectToRanking(matchId));
-    },
-  };
-};
+export default adapter => ({
+  getMatchByMatchId(matchId) {
+    return adapter.get(`${MATCH_API}/matches/${matchId}`)
+      .then(({ data: match }) => match);
+  },
+  getPositionsByMatchId(matchId) {
+    return adapter.get(`${MATCH_API}/matches/${matchId}/positions`)
+      .then(({ data: positions }) => positions);
+  },
+  postApplicantRankingByMatchId(matchId, applicantRanking) {
+    return adapter.post(`${MATCH_API}/matches/${matchId}/ranking`, applicantRanking)
+      .then(({ status }) => status === 200 && redirectToRanking(matchId));
+  },
+});
