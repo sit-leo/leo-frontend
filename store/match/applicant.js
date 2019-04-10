c90b138ea8edc2b27f697c3a3a9a8a9f9d7269d6
Keerati <keerati.jear@gmail.com>
2019-04-10 12:08:04 +0700

    [UPDATE] update code to compatable in new rank format

--- store/match/applicant.js
+++ store/match/applicant.js
@@ -11,24 +11,30 @@ const REMOVE_RANK = 'leo/match/remove/rank';
 // Reducer
 export default function reducer(state = initState, action = {}) {
   switch (action.type) {
-    case SET_RANK: return { ...state, ranks: action.ranks };
+    case SET_RANK: {
+      const ranks = [...action.ranks].map(rank => ({ ...rank, positionId: rank.position.id }));
+      return { ...state, ranks };
+    }
     case ADD_RANK: {
+      const { position } = action;
       const ranks = [...state.ranks];
-      if (!ranks.find(rank => rank.id === action.rank.id)) {
-        ranks.push({ ...action.rank, positionId: action.rank.id, sequence: ranks.length + 1 });
+      if (!ranks.find(rank => rank.positionId === position.id)) {
+        ranks.push({ position, positionId: position.id, sequence: ranks.length + 1 });
       }
       return { ...state, ranks };
     }
     case UPDATE_RANK: {
+      const { index, position } = action;
       let ranks = [...state.ranks];
-      const removalIndex = ranks.findIndex(rank => rank.id === action.rank.id);
+      const removalIndex = ranks.findIndex(rank => rank.positionId === position.positionId);
       ranks.splice(removalIndex, 1);
-      ranks.splice(action.index, 0, { ...action.rank, sequence: action.index + 1 });
+      ranks.splice(index, 0, { ...position, sequence: index + 1 });
       ranks = ranks.map((rank, rankIndex) => ({ ...rank, sequence: rankIndex + 1 }));
       return { ...state, ranks };
     }
     case REMOVE_RANK: {
-      let ranks = [...state.ranks].filter(rank => rank.name !== action.rank.name);
+      const { position } = action;
+      let ranks = [...state.ranks].filter(rank => rank.positionId !== position.positionId);
       ranks = ranks.map((rank, rankIndex) => ({ ...rank, sequence: rankIndex + 1 }));
       return { ...state, ranks };
     }
@@ -42,14 +48,14 @@ export function setRanks(ranks) {
   return { type: SET_RANK, ranks };
 }
 
-export function addRank(rank) {
-  return { type: ADD_RANK, rank };
+export function addRank(position) {
+  return { type: ADD_RANK, position };
 }
 
-export function updateRank(index, rank) {
-  return { type: UPDATE_RANK, index, rank };
+export function updateRank(index, position) {
+  return { type: UPDATE_RANK, index, position };
 }
 
-export function removeRank(rank) {
-  return { type: REMOVE_RANK, rank };
+export function removeRank(position) {
+  return { type: REMOVE_RANK, position };
 }
