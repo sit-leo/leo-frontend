09568c4ac6b665bfa3adc24b586e20f94f75c390
Keerati <keerati.jear@gmail.com>
2019-04-10 15:22:40 +0700

    [ADD] add finish component

---
+++ components/ranking/Finish.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from '../base/Card';
+import { TitleMedium } from '../base/Text';
+
+const Finish = () => (
+  <Card>
+    <TitleMedium>Ranking Finish :tada:</TitleMedium>
+  </Card>
+);
+
+export default Finish;
