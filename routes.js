94f9a41cce1780eed7e514e6fc1f81d876a5bb9a
Keerati <keerati.jear@gmail.com>
2019-04-10 14:50:52 +0700

    [ADD] add recruiter route

--- routes.js
+++ routes.js
@@ -2,4 +2,5 @@ const routes = require('next-routes');
 
 module.exports = routes()
   .add('matches/match', '/matches/:matchId')
-  .add('ranking/applicant', '/matches/:matchId/applicants/ranking');
+  .add('ranking/applicant', '/matches/:matchId/applicants/ranking')
+  .add('ranking/recruiter', '/matches/:matchId/recruiters/ranking');
