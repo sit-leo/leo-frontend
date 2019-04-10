2fbbf071b4d0192f2aab8afa51e830b22ee9a888
Keerati <keerati.jear@gmail.com>
2019-04-10 15:46:25 +0700

    [ADD] add recruiter page and store

---
+++ pages/ranking/recruiter.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { serverInstance } from '../../tools/request';
+import cookie from '../../tools/cookie';
+import adapter from '../../store/match/match-adapter';
+
+import { setMatch } from '../../store/match';
+import { setApplicants, setRanks, setIsUpdateRank } from '../../store/match/recruiter';
+
+import RecruiterRanking from '../../components/ranking/RecruiterRanking';
+
+class RecruiterRankingPage extends React.Component {
+  static async getInitialProps({ store, query, req }) {
+    const matchAdapter = adapter(serverInstance(cookie.getToken(req)));
+    const match = await matchAdapter.getMatchByMatchId(query.matchId);
+    const applicants = await matchAdapter.getPositionsByMatchId(query.matchId);
+    const ranks = await matchAdapter.getRecruiterRankingByMatchId(query.matchId);
+    await store.dispatch(setMatch(match));
+    await store.dispatch(setApplicants(applicants));
+    if (ranks && ranks.length > 0) {
+      await store.dispatch(setRanks(ranks));
+      await store.dispatch(setIsUpdateRank(true));
+    }
+    return {};
+  }
+
+  render() {
+    return <RecruiterRanking />;
+  }
+}
+
+export default connect()(RecruiterRankingPage);
---
+++ store/match/recruiter.js
@@ -0,0 +1,75 @@
+const initState = {
+  isUpdateRank: false,
+  applicants: [],
+  ranks: [],
+};
+
+  // Actions
+const SET_APPLICANTS = 'leo/match/recruiter/set/applicants';
+
+const SET_IS_UPDATE_RANK = 'leo/match/recruiter/set/isUpdateRank';
+const SET_RANK = 'leo/match/recruiter/set/rank';
+const ADD_RANK = 'leo/match/recruiter/add/rank';
+const UPDATE_RANK = 'leo/match/recruiter/update/rank';
+const REMOVE_RANK = 'leo/match/recruiter/remove/rank';
+
+// Reducer
+export default function reducer(state = initState, action = {}) {
+  switch (action.type) {
+    case SET_IS_UPDATE_RANK: return { ...state, isUpdateRank: action.isUpdateRank };
+    case SET_RANK: {
+      const ranks = [...action.ranks].map(rank => ({ ...rank, positionId: rank.position.id }));
+      return { ...state, ranks };
+    }
+    case ADD_RANK: {
+      const { position } = action;
+      const ranks = [...state.ranks];
+      if (!ranks.find(rank => rank.positionId === position.id)) {
+        ranks.push({ position, positionId: position.id, sequence: ranks.length + 1 });
+      }
+      return { ...state, ranks };
+    }
+    case UPDATE_RANK: {
+      const { index, position } = action;
+      let ranks = [...state.ranks];
+      const removalIndex = ranks.findIndex(rank => rank.positionId === position.positionId);
+      ranks.splice(removalIndex, 1);
+      ranks.splice(index, 0, { ...position, sequence: index + 1 });
+      ranks = ranks.map((rank, rankIndex) => ({ ...rank, sequence: rankIndex + 1 }));
+      return { ...state, ranks };
+    }
+    case REMOVE_RANK: {
+      const { position } = action;
+      let ranks = [...state.ranks].filter(rank => rank.positionId !== position.positionId);
+      ranks = ranks.map((rank, rankIndex) => ({ ...rank, sequence: rankIndex + 1 }));
+      return { ...state, ranks };
+    }
+    default: return { ...state };
+  }
+}
+
+// Action Creators
+
+export function setApplicants(applicants) {
+  return { type: SET_APPLICANTS, applicants };
+}
+
+export function setIsUpdateRank(isUpdateRank) {
+  return { type: SET_IS_UPDATE_RANK, isUpdateRank };
+}
+
+export function setRanks(ranks) {
+  return { type: SET_RANK, ranks };
+}
+
+export function addRank(position) {
+  return { type: ADD_RANK, position };
+}
+
+export function updateRank(index, position) {
+  return { type: UPDATE_RANK, index, position };
+}
+
+export function removeRank(position) {
+  return { type: REMOVE_RANK, position };
+}
