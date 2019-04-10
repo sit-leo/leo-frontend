41efce2c85d6411f686713a8781f0cc345d664af
Keerati <keerati.jear@gmail.com>
2019-04-10 11:34:10 +0700

    [UPDATE] update add rank test

--- store/match/__tests__/applicant.test.js
+++ store/match/__tests__/applicant.test.js
@@ -2,18 +2,22 @@ import ApplicantReducer from '../applicant';
 
 describe('Test Applicant Reducer', () => {
   it('Test addRank should return ranks that include added rank.', (done) => {
-    const rank = { id: 1, name: 'Software Developer' };
-    const action = { type: 'leo/match/add/rank', rank };
+    const position = { id: 1, name: 'Software Developer' };
+    const action = { type: 'leo/match/add/rank', position };
 
     const store = ApplicantReducer({ ranks: [] }, action);
 
-    expect(store).toEqual({ ranks: [{ ...rank, positionId: rank.id, sequence: 1 }] });
+    expect(store).toEqual({ ranks: [{ position, positionId: position.id, sequence: 1 }] });
     done();
   });
 
   it('Test addRank with same rank should not added rank.', (done) => {
     const rankAdded = {
-      id: 1, name: 'Software Developer', positionId: 1, sequence: 1,
+      positionId: 1,
+      sequence: 1,
+      position: {
+        id: 1, name: 'Software Developer',
+      },
     };
     const action = { type: 'leo/match/add/rank', rank: rankAdded };
 
