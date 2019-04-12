d2389ff905367ced1e76efb1682dd1250d1587e1
Keerati <keerati.jear@gmail.com>
2019-04-12 15:05:32 +0700

    [UPDATE] update route for position and recruiter ranking

--- routes.js
+++ routes.js
@@ -3,4 +3,5 @@ const routes = require('next-routes');
 module.exports = routes()
   .add('matches/match', '/matches/:matchId')
   .add('ranking/applicant', '/matches/:matchId/applicants/ranking')
-  .add('ranking/recruiter', '/matches/:matchId/recruiters/ranking');
+  .add('ranking/position', '/matches/:matchId/recruiters/positions')
+  .add('ranking/recruiter', '/matches/:matchId/positions/:positionId/ranking');
