d73222513ad1d11fb14271db6b3ae94cc2ea53d4
Keerati <keerati.jear@gmail.com>
2019-04-12 21:48:07 +0700

    [ADD] add recruiter test

---
+++ store/match/__tests__/recruiter.test.js
@@ -0,0 +1,13 @@
+import RecruiterReducer from '../recruiter';
+
+describe('Test Recruiter Reducer', () => {
+  it('Test ... should return ... correctly.', (done) => {
+    // const match = { id: 1, name: 'SIT Career Day' };
+    // const action = { type: 'leo/match/set/match', match };
+
+    // const store = RecruiterReducer({}, action);
+
+    // expect(store).toEqual({ match });
+    // done();
+  });
+});
