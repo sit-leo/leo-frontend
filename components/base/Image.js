a5bc5b73f7c5f4a51e6237d56fdfb21036d72361
Keerati <keerati.jear@gmail.com>
2019-04-16 22:45:19 +0700

    [ADD] add image and avartar component

---
+++ components/base/Image.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Image = styled.img`
+    max-width: 100px;
+    max-height: 100px;
+`;
+
+export default Image;
+
+export const Avatar = styled.img`
+    max-width: 135px;
+    max-height: 135px;
+`;
