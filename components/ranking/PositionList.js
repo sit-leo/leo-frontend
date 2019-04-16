0baf158b2c796687a877794541f8e2a1c8818412
Keerati <keerati.jear@gmail.com>
2019-04-17 00:39:00 +0700

    [UPDATE] update position list to use position with add to rank button

--- components/ranking/PositionList.js
+++ components/ranking/PositionList.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-import Card from '../base/Card';
+import { addRank } from '../../store/match/applicant';
+
+import { Col } from '../base/Grid';
+import Button from '../base/Button';
+
 import Position from './Position';
 
-const PositionList = ({ positions = [{ name: 'No Position Found', capacity: 0 }] }) => (
-  <React.Fragment>
+const PositionList = ({
+  addRank: addRanking = () => {},
+  positions = [{ name: 'No Position Found', capacity: 0 }],
+}) => (
+  <Col>
     {
-        positions.map((position => <Position key={position.id} position={position} />))
+        positions.map((position => (
+          <Position
+            key={position.id}
+            position={position}
+            actionButton={(
+              <Button
+                className="mt-2"
+                type="button"
+                onClick={() => addRanking(position)}
+              >
+                Add to rank
+              </Button>
+            )}
+          />
+        )))
     }
-  </React.Fragment>
+  </Col>
 );
 
 const mapStateToProps = state => ({
   positions: state.applicant.positions,
 });
 
-export default connect(mapStateToProps)(PositionList);
+const mapDispatchToPositionProps = dispatch => ({
+  addRank: bindActionCreators(addRank, dispatch),
+});
+
+
+export default connect(mapStateToProps, mapDispatchToPositionProps)(PositionList);
