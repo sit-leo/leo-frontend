7dd95fefc694c7b96639d7ebc590a5f126aa3a53
Keerati <keerati.jear@gmail.com>
2019-04-17 00:38:31 +0700

    [UPDATE] update position available to use custom button

--- components/ranking/Position.js
+++ components/ranking/Position.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 
 import { Collapse } from 'reactstrap';
 
 import Paragraph from 'antd/lib/typography/Paragraph';
-import { addRank } from '../../store/match/applicant';
 
-import ContainerRow, { Row, Col } from '../base/Grid';
+import ContainerRow, { Col } from '../base/Grid';
 import { TitleMedium, TextSmall } from '../base/Text';
 import { Avatar } from '../base/Image';
 import { SmallCard } from '../base/Card';
 import Flex, { FlexCenter } from '../base/Flex';
-import Button, { CardButton } from '../base/Button';
+import { CardButton } from '../base/Button';
 import color from '../../config/color';
 
 const InformationCollapse = styled(Collapse)`
@@ -23,34 +20,33 @@ const InformationCollapse = styled(Collapse)`
   width: 100%;
 `;
 
-const Position = ({ position, addRank: addRanking = () => {} }) => {
+const Position = ({
+  position,
+  actionButton,
+  rankingButton,
+}) => {
   const [isOpen, toggle] = useState(false);
   return (
     <SmallCard>
       <ContainerRow className="pt-4">
-        <Col lg={2}>
-          <FlexCenter className="flex-column">
+        <Col lg={rankingButton ? 3 : 2}>
+          <FlexCenter>
+            { rankingButton }
             <Avatar className="rounded-circle" src="/static/images/leo.png" alt="leo-logo" />
           </FlexCenter>
         </Col>
-        <Col lg={7}>
+        <Col lg={rankingButton ? 6 : 7}>
           <Flex className="flex-column justify-content-center text-left">
             <TitleMedium>{position.name}</TitleMedium>
-            <TextSmall>{position.salary || '฿15,000 - ฿20,000'}</TextSmall>
-            <TextSmall>{position.location || 'Company name co., Ltd - Phayathai, BKK'}</TextSmall>
+            <TextSmall>{position.salary}</TextSmall>
+            <TextSmall>{position.location}</TextSmall>
           </Flex>
         </Col>
         <Col lg={3}>
           <FlexCenter className="flex-column">
-            <TitleMedium className="mb-0">{position.capacity}</TitleMedium>
+            <TitleMedium className="mb-0">{position.capacity || 0}</TitleMedium>
             <TextSmall>Recruit</TextSmall>
-            <Button
-              className="mt-2"
-              type="button"
-              onClick={() => addRanking(position)}
-            >
-              Add to rank
-            </Button>
+            { actionButton }
           </FlexCenter>
         </Col>
 
@@ -75,8 +71,4 @@ const Position = ({ position, addRank: addRanking = () => {} }) => {
   );
 };
 
-const mapDispatchToPositionProps = dispatch => ({
-  addRank: bindActionCreators(addRank, dispatch),
-});
-
-export default connect(null, mapDispatchToPositionProps)(Position);
+export default Position;
