09a2633d886681b2176d44272010f35b66303286
Keerati <keerati.jear@gmail.com>
2019-04-17 00:26:54 +0700

    [ADD] add icon to base component

---
+++ components/base/Icon.js
@@ -0,0 +1,15 @@
+
+
+import styled from 'styled-components';
+
+import { Icon } from 'antd';
+
+import color from '../../config/color';
+import font from '../../config/font';
+
+export default Icon;
+
+export const DeletedIcon = styled(Icon)`
+    font-size: ${font.size.medium};
+    color: ${color.error};
+`;
