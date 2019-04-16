91f299fb43e0c6f8414a87ca3cdf763804aec3dd
Keerati <keerati.jear@gmail.com>
2019-04-16 21:33:30 +0700

    [UPDATE] update to use navbar without container

--- components/layouts/with-navbar.js
+++ components/layouts/with-navbar.js
@@ -1,13 +1,10 @@
 import React, { Fragment } from 'react';
 
-import { ContainerFluid } from '../base/Grid';
 import Navbar from '../base/Navbar';
 
 const WithNavbar = ({ children }) => (
   <Fragment>
-    <ContainerFluid>
-      <Navbar />
-    </ContainerFluid>
+    <Navbar />
     { children }
   </Fragment>
 );
