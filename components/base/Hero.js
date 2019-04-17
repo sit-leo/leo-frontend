45d3914d6f0fba4a0d0dff2aec9284ddb8685802
Keerati <keerati.jear@gmail.com>
2019-04-18 01:16:41 +0700

    [UPDATE] update hero component

--- components/base/Hero.js
+++ components/base/Hero.js
@@ -2,20 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 import color from '../../config/color';
-import font from '../../config/font';
 
-import { FlexCenter } from './Flex';
-
-const HeroContainer = styled(FlexCenter)`
-  background: ${color.white};
-  font-size: ${font.size.hero};
+const HeroContainer = styled.div`
+  background-color: ${color.white};
   min-height: 215px;
-  box-shadow: 0 2px 50px 0 ${color.shadow};
+  box-shadow: 0 25px 50px 0 ${color.shadow};
 `;
 
-const Hero = ({ text }) => (
+const Hero = ({ children }) => (
   <HeroContainer>
-    { text }
+    { children }
   </HeroContainer>
 );
 
