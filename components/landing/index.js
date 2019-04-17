a7270bb081dd002a6126a1ff54c95293b639bc84
Keerati <keerati.jear@gmail.com>
2019-04-18 01:00:29 +0700

    [UPDATE] update landing page

--- components/landing/index.js
+++ components/landing/index.js
@@ -2,17 +2,39 @@ import React from 'react';
 
 import env from '../../config/env';
 
-import { ContainerFluid } from '../base/Grid';
+import WithNavbar from '../layouts/with-navbar';
+
+import { ContainerFluid, Row, Col } from '../base/Grid';
 import { TitlePrimary, TextError } from '../base/Text';
+import Card from '../base/Card';
+import { RankingAvatar } from '../base/Image';
+import Flex, { FlexCenter } from '../base/Flex';
 
 const LandingIndex = () => (
-  <ContainerFluid>
-    <TitlePrimary>Hello LEO App !</TitlePrimary>
-    <TextError>{ env.public.type }</TextError>
-    <br />
-    <TextError>{ env.public.matchingApi }</TextError>
-    <hr />
-  </ContainerFluid>
+  <WithNavbar>
+    <ContainerFluid>
+      <Row>
+        <Col className="my-5">
+          <Card style={{ minHeight: '25vh' }}>
+            <FlexCenter className="flex-column">
+              <RankingAvatar src="/static/images/leo.png" />
+              <TitlePrimary>We are LEO!</TitlePrimary>
+            </FlexCenter>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            ENV Debugger
+            <hr />
+            <Flex className="flex-column">
+              <TextError>{ env.public.type }</TextError>
+              <TextError>{ env.public.matchingApi }</TextError>
+            </Flex>
+          </Card>
+        </Col>
+      </Row>
+    </ContainerFluid>
+  </WithNavbar>
 );
 
 export default LandingIndex;
