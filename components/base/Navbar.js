b474e14558da965767ad3e7428015b2fe482f514
Keerati <keerati.jear@gmail.com>
2019-04-18 00:54:32 +0700

    [UPDATE] update navbar style

--- components/base/Navbar.js
+++ components/base/Navbar.js
@@ -1,16 +1,67 @@
 import React from 'react';
 import styled from 'styled-components';
+import Link from 'next/link';
 
 import color from '../../config/color';
 
-const NavbarContainer = styled.div`
-  background: ${color.white};
-  min-height: 132px;
+import { ContainerStyled, Col, Row } from './Grid';
+import Image, { ProfileAvatar } from './Image';
+import { TitleMedium } from './Text';
+
+const Menu = styled(TitleMedium)`
+  cursor: pointer;
+  color: ${color.primary};
+  margin-right: 1.3em;
 `;
 
+const NavbarContainer = styled(ContainerStyled)`
+  background: ${color.white};
+  border: solid 0px ${color.disabled};
+  border-bottom-width: 2px;
+  & , .logo, .profile-avatar {
+    min-height: 132px;
+  }
+  .logo, .profile-avatar {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+const menus = [
+  {
+    name: 'Matches',
+    path: '/matches',
+  },
+  {
+    name: 'Applicant Ranking',
+    path: '/matches/1/applicants/ranking',
+  },
+  {
+    name: 'Recruiter Ranking',
+    path: '/matches/1/positions/1/ranking',
+  },
+];
+
 const Navbar = () => (
-  <NavbarContainer>
-    Navbar
+  <NavbarContainer fluid className="d-flex justify-content-center align-items-stretch">
+    <Row className="w-100 d-flex align-items-end">
+      <Col xs={6} lg={2} className="logo text-left">
+        <Image src="/static/images/leo.png" />
+      </Col>
+      <Col lg={8} className="mb-3 d-none d-lg-flex">
+        {
+          menus.map(menu => (
+            <Link href={menu.path}>
+              <Menu>{menu.name}</Menu>
+            </Link>
+          ))
+        }
+      </Col>
+      <Col xs={6} lg={2} className="profile-avatar text-right">
+        <ProfileAvatar className="rounded-circle" src="/static/images/avatar.png" />
+      </Col>
+    </Row>
   </NavbarContainer>
 );
 
