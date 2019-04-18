acf27deb13308d33ac12109a787db1b64fda7b3b
Keerati <keerati.jear@gmail.com>
2019-04-19 03:49:46 +0700

    [ADD] add ranking card to storybook

--- .storybook/stories/base/card.js
+++ .storybook/stories/base/card.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import ContainerRow, { Col } from '../../../components/base/Grid'
+
 import Card, { SmallCard } from '../../../components/base/Card';
+import Button from '../../../components/base/Button';
+
+import RankingCard from '../../../components/matching/RankingCard';
 
 storiesOf('Base/Card', module)
   .add('Card Component', () => (
@@ -9,4 +14,32 @@ storiesOf('Base/Card', module)
   ))
   .add('Small Card Component', () => (
     <SmallCard>Example Small Card</SmallCard>
+  ))
+  .add('Ranking Card Component', () => (
+    <ContainerRow>
+      <Col>
+        <RankingCard
+          title='Software Engineer'
+          value='฿15,000 - ฿20,000'
+          subtitle='Company name co., Ltd - Phayathai, BKK'
+          capacity={3}
+          actionButton={
+            <Button>Add to rank</Button>
+          }
+        />
+      </Col>
+      <Col>
+        <RankingCard
+          title='Jirapa Songchom'
+          value='GPAX 3.18'
+          subtitle='School of Information Technology, KMUTT'
+          capacity={3}
+          imagePath={'/static/images/avatar.png'}
+          badgeText='Documents'
+          actionButton={
+            <Button>Add to rank</Button>
+          }
+        />
+      </Col>
+    </ContainerRow>
   ))
