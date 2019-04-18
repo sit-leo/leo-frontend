313aa880dd86a58028715072d84416406f097a7d
Keerati <keerati.jear@gmail.com>
2019-04-19 03:41:56 +0700

    [UPDATE] update storybook global mock redux

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,12 +2,24 @@ import 'antd/dist/antd.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './font.css';
 
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+import { initState as match } from '../store/match'
+import { initState as ranking } from '../store/matching/ranking'
 
 const stories = require.context('./stories', true, /.js$/);
 
 function loadStories() {
   stories.keys().forEach(filename => stories(filename));
 }
+
+const mockStore = configureStore();
+const store = mockStore({
+  match,
+  ranking,
+});
+addDecorator(getStory => <Provider store={store}>{getStory()}</Provider>)
 
 configure(loadStories, module);
--- .storybook/stories/match/index.js
+++ .storybook/stories/match/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-
-import { initState as match } from '../../../store/match'
 
 import MatchPage from '../../../components/matches/MatchPage';
 
-const mockStore = configureStore();
-const store = mockStore({
-  match,
-});
-
 storiesOf('Match', module)
-  .addDecorator(getStory => <Provider store={store}>{getStory()}</Provider>)
   .add('MatchPage Component', () => (
     <MatchPage />
   ))
