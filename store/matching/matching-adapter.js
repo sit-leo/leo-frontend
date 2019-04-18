8004d8e979a6d4c907468a649255eaa5fda043f9
Keerati <keerati.jear@gmail.com>
2019-04-18 21:11:58 +0700

    [UPDATE] update match adapter to matching adapter group by domain

--- store/matching/matching-adapter.js
+++ store/matching/matching-adapter.js
@@ -2,7 +2,7 @@ import Router from 'next/router';
 
 import env from '../../config/env';
 
-const MATCH_API = env.public.matchingApi;
+const MATCHING_API = env.public.matchingApi;
 
 function redirectToApplicantRanking(matchId) {
   return Router.push(`/matches/${matchId}/applicants/ranking`);
@@ -14,43 +14,43 @@ export function redirectToRecruiterRanking(matchId, positionId) {
 
 export default adapter => ({
   getMatchByMatchId(matchId) {
-    return adapter.get(`${MATCH_API}/matches/${matchId}`)
+    return adapter.get(`${MATCHING_API}/matches/${matchId}`)
       .then(({ data: match }) => match);
   },
   getMatchPositionsByMatchId(matchId) {
-    return adapter.get(`${MATCH_API}/matches/${matchId}/positions`)
+    return adapter.get(`${MATCHING_API}/matches/${matchId}/positions`)
       .then(({ data: positions }) => positions);
   },
   postApplicantRankingByMatchId(matchId, applicantRanking) {
-    return adapter.post(`${MATCH_API}/matches/${matchId}/applicants/ranking`, applicantRanking)
+    return adapter.post(`${MATCHING_API}/matches/${matchId}/applicants/ranking`, applicantRanking)
       .then(({ status }) => status === 200 && redirectToApplicantRanking(matchId));
   },
   updateApplicantRankingByMatchId(matchId, applicantRanking) {
-    return adapter.put(`${MATCH_API}/matches/${matchId}/applicants/ranking`, applicantRanking)
+    return adapter.put(`${MATCHING_API}/matches/${matchId}/applicants/ranking`, applicantRanking)
       .then(({ status }) => status === 200 && redirectToApplicantRanking(matchId));
   },
   getApplicantRankingByMatchId(matchId) {
-    return adapter.get(`${MATCH_API}/matches/${matchId}/applicants/ranking`)
+    return adapter.get(`${MATCHING_API}/matches/${matchId}/applicants/ranking`)
       .then(({ data: ranks }) => ranks);
   },
   getRecruiterPositionsByMatchId(matchId) {
-    return adapter.get(`${MATCH_API}/matches/${matchId}/recruiters/positions`)
+    return adapter.get(`${MATCHING_API}/matches/${matchId}/recruiters/positions`)
       .then(({ data: positions }) => positions);
   },
   getApplicantsByMatchIdAndPositionId(matchId, positionId) {
-    return adapter.get(`${MATCH_API}/matches/${matchId}/positions/${positionId}/applicants`)
+    return adapter.get(`${MATCHING_API}/matches/${matchId}/positions/${positionId}/applicants`)
       .then(({ data: applicants }) => applicants);
   },
   getRecruiterRankingByMatchIdAndPositionId(matchId, positionId) {
-    return adapter.get(`${MATCH_API}/matches/${matchId}/recruiters/positions/${positionId}/ranking`)
+    return adapter.get(`${MATCHING_API}/matches/${matchId}/recruiters/positions/${positionId}/ranking`)
       .then(({ data: ranks }) => ranks);
   },
   postRecruiterRankingByMatchIdAndPositionId(matchId, positionId, recruiterRanking) {
-    return adapter.post(`${MATCH_API}/matches/${matchId}/positions/${positionId}/ranking`, recruiterRanking)
+    return adapter.post(`${MATCHING_API}/matches/${matchId}/positions/${positionId}/ranking`, recruiterRanking)
       .then(({ status }) => status === 200);
   },
   updateRecruiterRankingByMatchIdAndPositionId(matchId, positionId, recruiterRanking) {
-    return adapter.put(`${MATCH_API}/matches/${matchId}/positions/${positionId}/ranking`, recruiterRanking)
+    return adapter.put(`${MATCHING_API}/matches/${matchId}/positions/${positionId}/ranking`, recruiterRanking)
       .then(({ status }) => status === 200);
   },
 });
