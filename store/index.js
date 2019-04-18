f67dd3c2a949c5b6125429e302c0bf350c3b3b86
Keerati <keerati.jear@gmail.com>
2019-04-18 21:27:04 +0700

    [UPDATE] update reducers path and remove unused store

--- store/index.js
+++ store/index.js
@@ -3,17 +3,16 @@ import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import user from './account/user';
 import match from './match';
-import applicant from './match/applicant';
-import recruiter from './match/recruiter';
+import applicant from './matching/applicant';
+import recruiter from './matching/recruiter';
 
 const logger = createLogger();
 
 const middleware = [thunk, logger];
 
 export const reducers = combineReducers({
-  match, applicant, recruiter, user,
+  match, applicant, recruiter,
 });
 
 function initialStore(initialState = {}) {
