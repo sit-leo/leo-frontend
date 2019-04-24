3a5bb082389a488178e05e0269d78435ef3192e4
Keerati <keerati.jear@gmail.com>
2019-04-25 01:09:34 +0700

    [UPDATE] update redirect when logged in

--- store/user/user-adapter.js
+++ store/user/user-adapter.js
@@ -1,13 +1,18 @@
-// import Router from 'next/router';
+import Router from 'next/router';
 
 import env from '../../config/env';
+import cookie from '../../tools/cookie';
 
 const USER_API = env.public.userApi;
 
 export default adapter => ({
   login(credential) {
     return adapter.post(`${USER_API}/login`, credential)
-      .then(({ data: jwt }) => jwt);
+      .then(({ data: jwt }) => {
+        cookie.setToken(jwt.token);
+        Router.push('/matches/1/applicants/ranking');
+        return jwt;
+      });
   },
   getUser(token) {
     return adapter.get(
