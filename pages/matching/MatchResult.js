4b7e01431b0edb47a005b735e2fc5024a3ccd2eb
Keerati <keerati.jear@gmail.com>
2019-04-26 23:58:45 +0700

    [ADD] add fetch match results   in controller

--- pages/matching/MatchResult.js
+++ pages/matching/MatchResult.js
@@ -5,17 +5,18 @@ import { withAuth } from '../../tools/with-auth';
 import { serverInstance } from '../../tools/request';
 import cookie from '../../tools/cookie';
 
-import adapter from '../../store/matching/matching-adapter';
+import matchingAdapter from '../../store/matching/matching-adapter';
 
-// import { setMatchResult } from '../../store/matching/ranking';
+import { setMatchResults } from '../../store/matching/ranking';
 
 import MatchResultPage from '../../components/matching/MatchResultPage';
 
 class MatchResultController extends React.Component {
-  static async getInitialProps({ store, req }) {
-    // const matchingAdapter = adapter(serverInstance(cookie.getToken(req)));
-    // const matchResults = await matchingAdapter.getMatchResult();
-    // await store.dispatch(setMatchResult(matchResults));
+  static async getInitialProps({ store, req, query }) {
+    const { matchId } = query;
+    const matchingRequest = matchingAdapter(serverInstance(cookie.getToken(req)));
+    const matchResults = await matchingRequest.getMatchResultByMatchId(matchId);
+    await store.dispatch(setMatchResults(matchResults));
     return {};
   }
 
