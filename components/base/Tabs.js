3f0ffe3746207c20f8570326c7a81469d135de3a
Keerati <keerati.jear@gmail.com>
2019-04-28 22:14:59 +0700

    [ADD] add tabs with custom color

---
+++ components/base/Tabs.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { Tabs as DefaultTabs } from 'antd';
+import color from '../../config/color';
+
+const { TabPane: DefaultTabPane } = DefaultTabs;
+
+const Tabs = styled(DefaultTabs)`
+    .ant-tabs-ink-bar {
+        background-color: ${color.primary};
+    }
+    
+    .ant-tabs-nav .ant-tabs-tab:hover,
+    .ant-tabs-nav .ant-tabs-tab-active,
+    .ant-tabs-nav .ant-tabs-tab-active:active,
+    .ant-tabs-nav .ant-tabs-tab-active:hover {
+        color: ${color.primary};
+    }
+`;
+
+export default Tabs;
+
+export const TabPane = styled(DefaultTabPane)`
+
+`;
--- components/matches/MyMatchesPage.js
+++ components/matches/MyMatchesPage.js
@@ -7,20 +7,32 @@ import ContainerRow, { Col, Row } from '../base/Grid';
 
 import Card from '../base/Card';
 import MatchCard from './MatchCard';
+import Tabs, { TabPane } from '../base/Tabs';
 
-const MyMatchPage = ({ matches = [] }) => (
+
+const MatchList = ({ matches = [] }) => (
+  matches.map(match => (
+    <MatchCard key={match.name} />
+  ))
+);
+
+const MyMatchPage = ({ matches }) => (
   <WithNavbar>
     <ContainerRow className="py-5">
       <Card className="px-0">
         <Row>
           <Col lg={{ size: 10, offset: 1 }}>
-          tabs
-            <br />
-            {
-            matches.map(match => (
-              <MatchCard key={match.name} />
-            ))
-          }
+            <Tabs defaultActiveKey="1" animated={false}>
+              <TabPane tab="Current" key="1">
+                <MatchList matches={matches} />
+              </TabPane>
+              <TabPane tab="History" key="2">
+                <MatchList matches={matches} />
+              </TabPane>
+              <TabPane tab="Result" key="3">
+                <MatchList matches={matches} />
+              </TabPane>
+            </Tabs>
           </Col>
         </Row>
       </Card>
