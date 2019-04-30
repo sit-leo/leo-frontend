714bfb2ade3901ec37df1688a3616aae4b54a597
Keerati <keerati.jear@gmail.com>
2019-04-30 11:39:18 +0700

    [UPDATE] update to show match result attribute in ranking card

--- components/matching/MatchResultPage.js
+++ components/matching/MatchResultPage.js
@@ -1,17 +1,59 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import MatchingLayout from '../layouts/matching';
+
 import { Col, Row } from '../base/Grid';
 import { Title, TitlePrimary } from '../base/Text';
 import Card from '../base/Card';
 
 import RankingCard from './RankingCard';
 
-const ResultList = ({ results = [1] }) => (
-  results.map(result => <RankingCard />)
+function getApplicant(result) {
+  return null;
+  // const { applicant: { applicant } } = result;
+  // if (applicant) {
+  //   if (applicant.educations.length === 0) {
+  //     applicant.educations = [{ gpax: '-', educationName: '-' }];
+  //   }
+  //   return applicant;
+  // }
+  // return null;
+}
+
+function getPosition(result) {
+  const { position } = result;
+  if (position) {
+    return position;
+  }
+  return null;
+}
+
+const ResultList = ({ matchResults = [] }) => (
+  matchResults.map((result) => {
+    const title = getApplicant(result)
+      ? getApplicant(result).name
+      : getPosition(result).name;
+    const value = getApplicant(result)
+      ? getApplicant(result).educations[0].gpax
+      : getPosition(result).money;
+    const subtitle = getApplicant(result)
+      ? getApplicant(result).educations[0].educationName
+      : getPosition(result).recruiter.location;
+    return (
+      <RankingCard
+        key={result.matchResultId}
+        title={title}
+        value={value}
+        subtitle={subtitle}
+      />
+    );
+  })
 );
 
-const MatchResultPage = () => (
+const MatchResultPage = ({
+  matchResults,
+}) => (
   <MatchingLayout>
     <Col>
       <Card>
@@ -21,7 +63,7 @@ const MatchResultPage = () => (
             <Title>You have matched with 3 Applicants!!!</Title>
           </Col>
           <Col lg={{ size: 10, offset: 1 }}>
-            <ResultList />
+            <ResultList matchResults={matchResults} />
           </Col>
         </Row>
       </Card>
@@ -29,4 +71,9 @@ const MatchResultPage = () => (
   </MatchingLayout>
 );
 
-export default MatchResultPage;
+const mapStateToProps = state => ({
+  match: state.match.match,
+  matchResults: state.ranking.matchResults,
+});
+
+export default connect(mapStateToProps)(MatchResultPage);
