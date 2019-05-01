8adec6611845f57f5d90733eb31c3ee36aa0f515
Keerati <keerati.jear@gmail.com>
2019-05-01 16:51:48 +0700

    [UPDATE] update matching layout

--- components/layouts/matching.js
+++ components/layouts/matching.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import color from '../../config/color';
 
 import WithNavbar from './with-navbar';
 
 import Hero from '../base/Hero';
-import ContainerRow from '../base/Grid';
+import { FlexCenter } from '../base/Flex';
+import ContainerRow, { Col } from '../base/Grid';
+import { Title, SubTitleSmall, TitlePrimary } from '../base/Text';
+import Icon from '../base/Icon';
+
+const CounterBox = ({ counter = 0, text }) => (
+  <FlexCenter className="flex-column">
+    <TitlePrimary>{counter}</TitlePrimary>
+    <SubTitleSmall>{text}</SubTitleSmall>
+  </FlexCenter>
+);
+
+const VerticalLine = styled.div`
+  width: 1px;
+  height: 82px;
+  border: solid 1px ${color.outline};
+`;
 
 const Time = () => (
   <Hero>
-    <img className="w-100" src="/static/images/time_mockup.png" alt="time-mockup" />
+    <ContainerRow>
+      <Col lg={6}>
+        <Title className="mb-3">
+          15th Matching
+        </Title>
+        <Title className="d-flex align-items-center">
+          <Icon type="calendar" style={{ fontSize: '30px' }} className="mr-2" />
+          Matching date: 15 Nov 2019
+        </Title>
+      </Col>
+      <Col lg={4} className="d-flex justify-content-between">
+        <CounterBox counter={20} text="Recruiters" />
+        <VerticalLine />
+        <CounterBox counter={84} text="Applicants" />
+      </Col>
+    </ContainerRow>
   </Hero>
 );
 
