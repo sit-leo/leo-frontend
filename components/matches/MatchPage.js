0ed9ba084590667d7e4210f67489cffd86d5f020
Keerati <keerati.jear@gmail.com>
2019-05-01 16:58:22 +0700

    [UPDATE] update match page to link match result

--- components/matches/MatchPage.js
+++ components/matches/MatchPage.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Router from 'next/router';
+
+import WithNavbar from '../layouts/matching';
+
+import Button from '../base/Button';
+import Card from '../base/Card';
+import ContainerRow, { Col } from '../base/Grid';
 
 const MatchPage = ({ match }) => (
-  <React.Fragment>
-    <h1>{match.name}</h1>
-  </React.Fragment>
+  <WithNavbar>
+    <Card>
+      <ContainerRow>
+        <Col className="text-center">
+          <h1>{match.name}</h1>
+        </Col>
+        <Col lg={{ size: 6, offset: 3 }} className="text-center">
+          <Button onClick={() => Router.push(`/matches/${match.id}/result`)}>Match Result</Button>
+        </Col>
+      </ContainerRow>
+    </Card>
+  </WithNavbar>
 );
 
 
