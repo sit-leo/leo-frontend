942f7467ff8b0f201b05a2b0e89a44e5266cb16a
Keerati <keerati.jear@gmail.com>
2019-05-01 11:47:47 +0700

    [ADD] add set role in match result

--- pages/matching/MatchResult.js
+++ pages/matching/MatchResult.js
@@ -6,7 +6,9 @@ import { serverInstance } from '../../tools/request';
 import cookie from '../../tools/cookie';
 
 import matchingAdapter from '../../store/matching/matching-adapter';
+import userAdapter from '../../store/user/user-adapter';
 
+import { setId, setRole } from '../../store/user';
 import { setMatchResults } from '../../store/matching/ranking';
 
 import MatchResultPage from '../../components/matching/MatchResultPage';
@@ -14,8 +16,14 @@ import MatchResultPage from '../../components/matching/MatchResultPage';
 class MatchResultController extends React.Component {
   static async getInitialProps({ store, req, query }) {
     const { matchId } = query;
+    const userRequest = userAdapter(serverInstance(cookie.getToken(req)));
     const matchingRequest = matchingAdapter(serverInstance(cookie.getToken(req)));
+
+    const user = await userRequest.getUser();
     const matchResults = await matchingRequest.getMatchResultByMatchId(matchId);
+
+    await store.dispatch(setId(user.id));
+    await store.dispatch(setRole(user.role));
     await store.dispatch(setMatchResults(matchResults));
     return {};
   }
