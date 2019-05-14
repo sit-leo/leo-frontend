a4754c76a20c77a32c169f81c8c5a66194b50eeb
Keerati <keerati.jear@gmail.com>
2019-05-14 23:48:36 +0700

    [ADD] fetch user in match page

--- pages/match/index.js
+++ pages/match/index.js
@@ -4,7 +4,9 @@ import { connect } from 'react-redux';
 import { serverInstance } from '../../tools/request';
 import cookie from '../../tools/cookie';
 import adapter from '../../store/match/match-adapter';
+import userAdapter from '../../store/user/user-adapter';
 
+import { setRole, setId } from '../../store/user';
 import { setMatch } from '../../store/match';
 
 import MatchPage from '../../components/matches/MatchPage';
@@ -13,6 +15,13 @@ class MatchController extends React.Component {
   static async getInitialProps({ store, query, req }) {
     const matchAdapter = adapter(serverInstance(cookie.getToken(req)));
     const match = await matchAdapter.getMatchByMatchId(query.matchId);
+    const userRequest = userAdapter(serverInstance(cookie.getToken(req)));
+
+
+    const user = await userRequest.getUser();
+
+    await store.dispatch(setId(user.id));
+    await store.dispatch(setRole(user.role));
     await store.dispatch(setMatch(match));
     return {};
   }
