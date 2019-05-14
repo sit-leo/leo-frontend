fdb16fcd1a127e9b8b4f80f9aa24a0aff8aefb83
Keerati <keerati.jear@gmail.com>
2019-05-14 23:34:24 +0700

    [ADD] add new page for select position before see match result

---
+++ components/matching/RecruiterPositionMatchResultPage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Router from 'next/router';
+import { connect } from 'react-redux';
+
+import RecruiterPosition from './RecruiterPosition';
+
+function redirectToMatchResult(matchId) {
+  return Router.push(`/matches/${matchId}/result`);
+}
+
+const RecruiterPositionMatchResultPage = ({
+  match,
+  positions = [],
+}) => (
+  <RecruiterPosition
+    match={match}
+    positions={positions}
+    redirect={redirectToMatchResult}
+    textAction="Choose position to see match result"
+  />
+);
+
+const mapStateToProps = state => ({
+  match: state.match.match,
+  positions: state.ranking.positions,
+});
+
+export default connect(mapStateToProps)(RecruiterPositionMatchResultPage);
---
+++ pages/matching/RecruiterPositionMatchResult.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { withAuth } from '../../tools/with-auth';
+import withUser from '../../tools/with-user';
+import withRole, { isRecruiter } from '../../tools/with-roles';
+
+import { serverInstance } from '../../tools/request';
+import cookie from '../../tools/cookie';
+
+import matchAdapter from '../../store/match/match-adapter';
+import matchingAdapter from '../../store/matching/matching-adapter';
+
+import { setMatch } from '../../store/match';
+import { setPositions } from '../../store/matching/ranking';
+
+import RecruiterPositionMatchResultPage from '../../components/matching/RecruiterPositionMatchResultPage';
+
+class RecruiterPositionMatchResultPageController extends React.Component {
+  static async getInitialProps({ store, query, req }) {
+    const matchRequest = matchAdapter(serverInstance(cookie.getToken(req)));
+    const matchingRequest = matchingAdapter(serverInstance(cookie.getToken(req)));
+
+    const { matchId } = query;
+    const match = await matchRequest.getMatchByMatchId(matchId);
+    const positions = await matchingRequest.getRecruiterPositionsByMatchId(matchId);
+
+    await store.dispatch(setMatch(match));
+    if (positions && positions.length > 0) {
+      await store.dispatch(setPositions(positions));
+    }
+    return {};
+  }
+
+  render() {
+    return <RecruiterPositionMatchResultPage />;
+  }
+}
+
+export default withRole(isRecruiter)(
+  withUser(
+    withAuth(
+      connect()(RecruiterPositionMatchResultPageController),
+    ),
+  ),
+);
