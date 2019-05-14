a3a2ee3526d727b3ae018519379d6c08b1d8e7d2
Keerati <keerati.jear@gmail.com>
2019-05-14 23:51:10 +0700

    [UPDATE] update replace set loading over load match on ssr

--- pages/match/MyMatches.js
+++ pages/match/MyMatches.js
@@ -4,20 +4,19 @@ import { connect } from 'react-redux';
 import { withAuth } from '../../tools/with-auth';
 import withUser from '../../tools/with-user';
 
-import { serverInstance } from '../../tools/request';
-import cookie from '../../tools/cookie';
+// import { serverInstance } from '../../tools/request';
+// import cookie from '../../tools/cookie';
 
-import matchAdapter from '../../store/match/match-adapter';
+// import matchAdapter from '../../store/match/match-adapter';
 
-import { setMatches } from '../../store/match';
+import { setLoading } from '../../store/global';
 
 import MyMatchesPage from '../../components/matches/MyMatchesPage';
 
 class MyMatchesController extends React.Component {
   static async getInitialProps({ store, req }) {
-    const matchRequest = matchAdapter(serverInstance(cookie.getToken(req)));
-    const matches = await matchRequest.getCurrentMatches();
-    await store.dispatch(setMatches(matches));
+    // const matchRequest = matchAdapter(serverInstance(cookie.getToken(req)));
+    await store.dispatch(setLoading(true));
     return {};
   }
 
