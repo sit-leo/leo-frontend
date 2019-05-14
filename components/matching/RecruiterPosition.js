dc7ce8394c3de0cf3467dc6743eb3a6b87848936
Keerati <keerati.jear@gmail.com>
2019-05-14 23:34:17 +0700

    [UPDATE] update recruiter to available to reuse

---
+++ components/matching/RecruiterPosition.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { Input } from 'antd';
+
+import MatchingLayout from '../layouts/matching';
+
+import { Col, Row } from '../base/Grid';
+import { TitleSmall } from '../base/Text';
+import Card from '../base/Card';
+import MainButton from '../base/Button';
+
+import RankingCard from './RankingCard';
+
+const PositionList = ({ match, positions, redirect }) => (
+  <div className="d-flex flex-column">
+    {
+      positions.map(position => (
+        <RankingCard
+          key={position.name}
+          title={position.name}
+          value={position.money}
+          subtitle={position.location}
+          capacity={position.capacity}
+          actionButton={(
+            <MainButton className="my-3 w-75" onClick={() => redirect(match.id, position.id)}>
+              Select
+            </MainButton>
+          )}
+        />
+      ))
+    }
+  </div>
+);
+
+const RecruiterPosition = ({
+  match,
+  positions = [],
+  textAction = 'Choose position to rank',
+  redirect = () => {},
+}) => (
+  <MatchingLayout>
+    <Col>
+      <Card>
+        <Row>
+          <Col lg={{ offset: 1, size: 6 }}>
+            <TitleSmall>{textAction}</TitleSmall>
+          </Col>
+          <Col lg={4}>
+            <Input />
+          </Col>
+          <Col lg={{ size: 10, offset: 1 }}>
+            <PositionList match={match} positions={positions} redirect={redirect} />
+          </Col>
+        </Row>
+      </Card>
+    </Col>
+  </MatchingLayout>
+);
+
+export default RecruiterPosition;
--- components/matching/RecruiterPositionPage.js
+++ components/matching/RecruiterPositionPage.js
@@ -1,57 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { Input } from 'antd';
 import { redirectToRecruiterRanking } from '../../store/matching/matching-adapter';
 
-import MatchingLayout from '../layouts/matching';
+import RecruiterPosition from './RecruiterPosition';
 
-import { Col, Row } from '../base/Grid';
-import { TitleSmall } from '../base/Text';
-import Card from '../base/Card';
-import MainButton from '../base/Button';
-
-import RankingCard from './RankingCard';
-
-const PositionList = ({ match, positions }) => (
-  <div className="d-flex flex-column">
-    {
-      positions.map(position => (
-        <RankingCard
-          key={position.name}
-          title={position.name}
-          value={position.money}
-          subtitle={position.location}
-          capacity={position.capacity}
-          actionButton={(
-            <MainButton className="my-3 w-75" onClick={() => redirectToRecruiterRanking(match.id, position.id)}>
-              Select
-            </MainButton>
-          )}
-        />
-      ))
-    }
-  </div>
-);
-
-const RecruiterPosition = ({ match, positions = [] }) => (
-  <MatchingLayout>
-    <Col>
-      <Card>
-        <Row>
-          <Col lg={{ offset: 1, size: 6 }}>
-            <TitleSmall>Choose position to rank</TitleSmall>
-          </Col>
-          <Col lg={4}>
-            <Input />
-          </Col>
-          <Col lg={{ size: 10, offset: 1 }}>
-            <PositionList match={match} positions={positions} />
-          </Col>
-        </Row>
-      </Card>
-    </Col>
-  </MatchingLayout>
+const RecruiterPositionPage = ({
+  match,
+  positions = [],
+}) => (
+  <RecruiterPosition match={match} positions={positions} redirect={redirectToRecruiterRanking} />
 );
 
 const mapStateToProps = state => ({
@@ -59,4 +17,4 @@ const mapStateToProps = state => ({
   positions: state.ranking.positions,
 });
 
-export default connect(mapStateToProps)(RecruiterPosition);
+export default connect(mapStateToProps)(RecruiterPositionPage);
