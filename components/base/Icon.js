21c0f4fe808928a868be4ef57c0227e3c10ecb70
Keerati <keerati.jear@gmail.com>
2019-05-25 14:11:14 +0700

    [ADD] add icon disabled

--- components/base/Icon.js
+++ components/base/Icon.js
@@ -7,10 +7,15 @@ import { Icon } from 'antd';
 import color from '../../config/color';
 import font from '../../config/font';
 
-export default Icon;
+export default styled(Icon)`
+    ${props => props.disabled && `
+        color: ${color.disabled};
+        cursor: not-allowed;
+    `}
+`;
 
 export const DeletedIcon = styled(Icon)`
-    font-size: ${font.size.medium};
+    font-size: ${font.size.large};
     color: ${color.error};
     cursor: pointer;
 `;
