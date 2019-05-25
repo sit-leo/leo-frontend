e79d30edc18fec6391b13f165de594786fd5d051
Keerati <keerati.jear@gmail.com>
2019-05-25 16:30:20 +0700

    [ADD] add check match result date error page

--- pages/matching/MatchResult.js
+++ pages/matching/MatchResult.js
@@ -5,8 +5,13 @@ import withUser from '../../tools/with-user';
 import { withAuth } from '../../tools/with-auth';
 import { serverInstance } from '../../tools/request';
 
+import { isAnnouceDate } from '../../tools/check-match-time';
+
+import redirectError from '../../tools/redirect-error';
+
 import cookie from '../../tools/cookie';
 
+import matchAdapter from '../../store/match/match-adapter';
 import matchingAdapter from '../../store/matching/matching-adapter';
 
 import { setMatchResults } from '../../store/matching/ranking';
@@ -14,9 +19,22 @@ import { setMatchResults } from '../../store/matching/ranking';
 import MatchResultPage from '../../components/matching/MatchResultPage';
 
 class MatchResultController extends React.Component {
-  static async getInitialProps({ store, req, query }) {
+  static async getInitialProps({
+    store, req, res, query,
+  }) {
     const { matchId } = query;
+    const matchRequest = matchAdapter(serverInstance(cookie.getToken(req)));
     const matchingRequest = matchingAdapter(serverInstance(cookie.getToken(req)));
+
+    const match = await matchRequest.getMatchByMatchId(matchId);
+
+    if (match.error) {
+      return redirectError({ req, res }, 'No Match Found.');
+    }
+
+    if (!isAnnouceDate(match.announceDate)) {
+      return redirectError({ req, res }, 'Announce Day not started or has ended.');
+    }
 
     const matchResults = await matchingRequest.getMatchResultByMatchId(matchId);
 
