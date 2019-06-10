8e84e73b2037402ab3e129ca1257c800e2e2c65a
Keerati <keerati.jear@gmail.com>
2019-06-10 22:45:22 +0700

    [UPDATE] update login page layout

--- components/user/LoginPage.js
+++ components/user/LoginPage.js
@@ -10,9 +10,10 @@ import userAdapter from '../../store/user/user-adapter';
 import { setUsername, setPassword } from '../../store/user';
 
 import { Title } from '../base/Text';
-import Button from '../base/Button';
+import { SmallButton, SmallMainButton } from '../base/Button';
 import Input from '../base/Input';
-import ContainerRow, { Col } from '../base/Grid';
+import ContainerRow, { Col, Row } from '../base/Grid';
+import { FlexCenter } from '../base/Flex';
 
 const userRequest = userAdapter(clientInstance());
 
@@ -22,27 +23,56 @@ const LoginPage = ({
   setUsername: handleUsername,
   setPassword: handlePassword,
 }) => (
-  <React.Fragment>
+  <FlexCenter className="vh-100">
     <ContainerRow>
-      <Col>
+      <Col className="py-3">
+        <Row>
+          <Col className="text-center text-lg-right" lg={{ size: 3, offset: 3 }}>
+            <img className="w-25" src="/static/images/leo.png" alt="LEO-Logo" />
+          </Col>
+          <Col className="text-center text-lg-left" lg={{ size: 3 }}>
+            <h1 className="m-0">LEO</h1>
+            Matching System
+          </Col>
+        </Row>
+      </Col>
+      <Col lg={{ size: 6, offset: 3 }}>
         <Form
           method="POST"
-          className="w-100 py-4"
+          className="w-100 py-4 px-4 card"
           onSubmit={(e) => {
             e.preventDefault();
             userRequest.login({ username, password });
           }}
         >
-          <Title>Login Page</Title>
-          Username
-          <Input onChange={e => handleUsername(e.target.value)} value={username} />
-          Password
-          <Input type="password" onChange={e => handlePassword(e.target.value)} value={password} />
-          <Button htmlType="submit" className="w-100 mt-4">Login</Button>
+          <Title>Login</Title>
+          <Input
+            onChange={e => handleUsername(e.target.value)}
+            value={username}
+            placeholder="Email"
+            required
+          />
+          <Input
+            type="password"
+            onChange={e => handlePassword(e.target.value)}
+            value={password}
+            placeholder="password"
+            required
+          />
+          <div className="text-right">
+            <a href="#forgot-password">
+              <u>Forgot Password?</u>
+            </a>
+          </div>
+          <SmallMainButton htmlType="submit" className="my-3 w-25 mx-auto">Login</SmallMainButton>
+          <div className="text-center">
+            <span className="mr-3">{`${"Don't have any account ?"}`}</span>
+            <SmallButton>Register</SmallButton>
+          </div>
         </Form>
       </Col>
     </ContainerRow>
-  </React.Fragment>
+  </FlexCenter>
 );
 
 const mapStateToProps = state => ({
