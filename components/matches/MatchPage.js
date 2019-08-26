70aaabe1351719d4c33069802ad447f341aeee45
Keerati <keerati.jear@gmail.com>
2019-08-26 21:55:59 +0700

    [UPDATE] style new ui for match page

--- components/matches/MatchPage.js
+++ components/matches/MatchPage.js
@@ -1,15 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import Router from 'next/router';
+import { Breadcrumb, Card as AntdCard, Avatar } from 'antd';
 
-import WithNavbar from '../layouts/matching';
+import colors from '../../config/color';
 
+import WithNavbar from '../layouts/with-navbar';
+
+
+import {
+  TitleLarge, Title, TitleWhite, TitleSmall, TitlePrimary,
+} from '../base/Text';
 import Button from '../base/Button';
 import Card from '../base/Card';
-import ContainerRow, { Col } from '../base/Grid';
+import ContainerRow, { Row, Col } from '../base/Grid';
 
 import { ROLE_APPLICANT } from '../../tools/with-roles';
+
+const Meta = styled(AntdCard.Meta)`
+  .ant-card-meta-title {
+    margin: 0 !important;
+  }
+`;
+
+const LabelCard = styled.div`
+  border-radius: 2px;
+  border: solid 1px ${colors.primary};
+  text-align: center;
+  padding: 0.5em;
+  margin-bottom: 1em;
+`;
+
+const DateLabel = ({ title, date }) => (
+  <Meta
+    className="my-3"
+    avatar={<Avatar shape="square" src="/static/images/calendar.png" />}
+    title={<TitleSmall className="m-0">{title}</TitleSmall>}
+    description={<TitleSmall>{date}</TitleSmall>}
+  />
+);
+
+const NumberLabel = ({ description, number }) => (
+  <LabelCard>
+    <TitlePrimary className="m-0">{number}</TitlePrimary>
+    <TitleSmall>{description}</TitleSmall>
+  </LabelCard>
+);
 
 const MatchPage = ({ match, role }) => {
   function handleMatchResult() {
@@ -20,18 +58,77 @@ const MatchPage = ({ match, role }) => {
   }
   return (
     <WithNavbar>
-      <Card>
-        <ContainerRow>
-          <Col className="text-center">
-            <h1>{match.name}</h1>
-          </Col>
-          <Col lg={{ size: 6, offset: 3 }} className="text-center">
-            <Button onClick={handleMatchResult}>
+      <ContainerRow className="py-5">
+        <Col>
+          <Breadcrumb>
+            <Breadcrumb.Item>
+              <a href="/matches">Matches</a>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              Junior Programmer Match
+            </Breadcrumb.Item>
+          </Breadcrumb>
+        </Col>
+        <Col lg={7} className="text-left">
+          <TitleLarge>Junior Programmer Match</TitleLarge>
+          <img className="w-100" src="/static/images/match-mockup.jpg" alt="match-mockup" />
+          <hr />
+          {
+            'This matching is only for Junior Programmer. Join to apply the work and recruit the great employee, rank them by yourself.'
+          }
+        </Col>
+        <Col lg={5}>
+          <Card>
+            <Row>
+              <Col>
+                <Title>Match Details</Title>
+              </Col>
+              <Col>
+                <DateLabel
+                  title="Join Period"
+                  date="15 - 21 November 2019 (end at 00:00)"
+                />
+              </Col>
+              <Col>
+                <DateLabel
+                  title="Applicant Rank"
+                  date="22 - 24 November 2019 (end at 00:00)"
+                />
+              </Col>
+              <Col>
+                <DateLabel
+                  title="Recruiter Rank"
+                  date="25 - 27 November 2019 (end at 00:00)"
+                />
+              </Col>
+              <Col>
+                <DateLabel
+                  title="Result"
+                  date="28 November 2019 (at 9:00)"
+                />
+              </Col>
+              <hr className="w-100" />
+              <Col lg={6}>
+                <NumberLabel description="Applicants" number="58" />
+              </Col>
+              <Col lg={6}>
+                <NumberLabel description="Recruiters" number="12" />
+              </Col>
+              <Col className="text-center">
+                <Button className="w-100" onClick={null}>
+                  <TitleWhite className="mb-0">Join Match</TitleWhite>
+                </Button>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+        <Col className="text-center">
+          <hr className="w-100" />
+          <Button onClick={handleMatchResult}>
             Match Result
-            </Button>
-          </Col>
-        </ContainerRow>
-      </Card>
+          </Button>
+        </Col>
+      </ContainerRow>
     </WithNavbar>
   );
 };
