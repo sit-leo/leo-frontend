2d536a06af74ce5f8fdfe55f2447032346092070
Keerati <keerati.jear@gmail.com>
2019-08-27 12:33:07 +0700

    [UPDATE] styled error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,18 +1,54 @@
 import React from 'react';
 
+import ContainerRow, { Col } from '../components/base/Grid';
+import { FlexCenter } from '../components/base/Flex';
+import Button from '../components/base/Button';
+import { TitlePrimary, TitleLight, TitleWhite } from '../components/base/Text';
+
 class Error extends React.Component {
   static getInitialProps({ res, err, query }) {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
     return { statusCode, query };
   }
 
   render() {
-    const { query: { message } } = this.props;
+    const { query: { message }, statusCode } = this.props;
     return (
-      <p>
-        {message && `${message}`}
-        <button onClick={() => window.history.back()}>Back</button>
-      </p>
+      <FlexCenter className="vh-100">
+        <ContainerRow>
+          <Col className="text-center">
+            <img className="w-25" alt="error-code" src={`/static/images/error_${statusCode}.png`} />
+          </Col>
+          <Col className="text-center mt-5">
+            <TitlePrimary>
+              {
+                statusCode === 404
+                  ? 'PAGE NOT FOUND'
+                  : 'SERVER ERROR'
+              }
+            </TitlePrimary>
+          </Col>
+          <Col lg={{ size: 10, offset: 1 }} className="text-center">
+            <TitleLight>
+              {
+                message
+                  ? `${message}`
+                  : `The requested resource could not be found but may be available in the future. 
+                  Subsequent requests by the client are permissible.`
+              }
+            </TitleLight>
+          </Col>
+          <Col className="text-center mt-5">
+            <Button>
+              <a href="/">
+                <TitleWhite className="mb-0">
+                  Go to home page
+                </TitleWhite>
+              </a>
+            </Button>
+          </Col>
+        </ContainerRow>
+      </FlexCenter>
     );
   }
 }
