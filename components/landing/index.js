5a42d76bb0ceda18624dafd6764089a5099b98b1
jirapa songchom <jirapa.songchom@mail.kmutt.ac.th>
2019-08-29 14:38:17 +0700

    [ADD] New Landing page and ContainerPrimaryStyled

--- components/landing/index.js
+++ components/landing/index.js
@@ -10,11 +10,13 @@ import { setRole, setId } from '../../store/user';
 import WithNavbar from '../layouts/with-navbar';
 
 import EnvDebugger from '../base/Debugger';
-import { ContainerFluid, Row, Col } from '../base/Grid';
-import { TitlePrimary } from '../base/Text';
+import ContainerRow, { ContainerFluidPrimary, Row, Col } from '../base/Grid';
+import { TitlePrimary, TitleLarge, TitleLight, TitleSmall, SubTitleSmall, TitleSmallWhite, ExtraSmallTextLight } from '../base/Text';
+import MainButton from '../base/Button';
 import Card from '../base/Card';
 import { RankingAvatar } from '../base/Image';
 import { FlexCenter } from '../base/Flex';
+import { from } from 'rxjs';
 
 const userRequest = userAdapter(clientInstance());
 
@@ -22,19 +24,54 @@ const LandingIndex = ({ role, setUser }) => {
   userRequest.getUser().then(user => setUser(user));
   return (
     <WithNavbar>
-      <ContainerFluid>
-        <Row>
-          <Col className="my-5">
-            <Card style={{ minHeight: '25vh' }}>
-              <FlexCenter className="flex-column">
-                <RankingAvatar src="/static/images/leo.png" />
-                <TitlePrimary>We are LEO!</TitlePrimary>
-              </FlexCenter>
-            </Card>
+      <ContainerRow>
+          <Col lg={5} className="my-5">
+            <TitleLarge>We are LEO</TitleLarge>
+            <TitleLight>A land that easy to find the opportunities.</TitleLight>
+            <SubTitleSmall className="my-5">You will get the best match here. Ranking them by yourself and let we match them for you. We guarantee the best result by using Deferred Acceptance Algorithm, awarded the Nobel prize of Economic Sciences in Memory of Alfred Nobel 2012.</SubTitleSmall>
+            <MainButton className="width-100">Register</MainButton>
           </Col>
+          <Col lg={7}>
+            <img className="w-100" src="/static/images/girl-boy-landing.png"/>
+          </Col>
+      </ContainerRow>
+      <ContainerFluidPrimary>
+        <ContainerRow className="py-5">
+          
+            <Col lg={4} className="py-3">
+            <center>
+              <img className="w-25" src="/static/images/leo.png"/>
+              <TitleSmall className="my-2">We do the matching for you.</TitleSmall>
+              <ExtraSmallTextLight className="my-2">We serve you the generic matching here.  </ExtraSmallTextLight>
+            </center>
+            </Col>
+
+            <Col lg={4} className="py-3">
+            <center>
+              <img className="w-25" src="/static/images/leo.png"/>
+              <TitleSmall className="my-2">You rank. Then, we match them.</TitleSmall>
+              <ExtraSmallTextLight className="my-2">We use your rank and match them with 
+the other rank by deferred acceptance algorithms  </ExtraSmallTextLight>
+            </center>
+            </Col>
+
+            <Col lg={4} className="py-3">
+            <center>
+              <img className="w-25" src="/static/images/leo.png"/>
+              <TitleSmall className="my-2">We use Deferred Acceptance Algorithm.</TitleSmall>
+              <ExtraSmallTextLight className="my-2">Awarded the Nobel prize of Economic Sciences in Memory of Alfred Nobel 2012.  </ExtraSmallTextLight>
+            </center>
+            </Col>
+            
+
+          
+        </ContainerRow>
+        
+        <Row>
+          
           <EnvDebugger role={role} />
         </Row>
-      </ContainerFluid>
+      </ContainerFluidPrimary>
     </WithNavbar>
   );
 };
