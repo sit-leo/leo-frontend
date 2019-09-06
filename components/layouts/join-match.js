fbe15758fe49b2b4b6cab4b48678b90ca61e98a1
Keerati <keerati.jear@gmail.com>
2019-09-06 13:26:37 +0700

    [ADD] implement join match layout

---
+++ components/layouts/join-match.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  Breadcrumb,
+} from 'antd';
+
+import WithNavbar from './with-navbar';
+
+import ContainerRow, { Col } from '../base/Grid';
+
+const JoinMatch = ({ children }) => (
+  <WithNavbar>
+    <ContainerRow className="my-3">
+      <Col>
+        <Breadcrumb>
+          <Breadcrumb.Item>
+            Matchings
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            Junior Programmer Match
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            Join matching
+          </Breadcrumb.Item>
+        </Breadcrumb>
+      </Col>
+      {children}
+    </ContainerRow>
+  </WithNavbar>
+);
+
+export default JoinMatch;
--- components/matches/ApplicantJoinMatchPage.js
+++ components/matches/ApplicantJoinMatchPage.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import {
-  Breadcrumb, Upload, Icon, Tag,
+  Upload, Icon, Tag,
 } from 'antd';
 
-import WithNavbar from '../layouts/with-navbar';
+import WithJoinMatch from '../layouts/join-match';
 
-import ContainerRow, { Col } from '../base/Grid';
-import { TitleLarge, TitleMedium, SubTitleSmallWhite } from '../base/Text';
+import { Col } from '../base/Grid';
+import { TitleLarge, TitleForm, SubTitleSmallWhite } from '../base/Text';
 import { LabelInput, TextArea } from '../base/Input';
 import { SmallMainButton } from '../base/Button';
-
-const TitleForm = ({ title }) => (
-  <Col>
-    <hr />
-    <TitleMedium className="mb-2">
-      <b>{title}</b>
-    </TitleMedium>
-  </Col>
-);
 
 const UploadButton = () => (
   <div>
@@ -27,86 +18,71 @@ const UploadButton = () => (
 );
 
 const ApplicantJoinMatchPage = () => (
-  <WithNavbar>
-    <ContainerRow className="my-3">
-      <Col>
-        <Breadcrumb>
-          <Breadcrumb.Item>
-            Matchings
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            Junior Programmer Match
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            Join matching
-          </Breadcrumb.Item>
-        </Breadcrumb>
-      </Col>
-      <Col>
-        <TitleLarge className="my-2">Junior Programmer Match</TitleLarge>
-      </Col>
-      <TitleForm title="Profile" />
-      <Col lg={6}>
-        <LabelInput label="Firstname" name="firstname" text="Jirapa" disabled />
-      </Col>
-      <Col lg={6}>
-        <LabelInput label="Lastname" name="lastname" text="Songchom" disabled />
-      </Col>
-      <Col lg={6}>
-        <LabelInput label="Email" name="email" text="jirapas.jil@gmail.com" disabled />
-      </Col>
-      <Col lg={6}>
-        <LabelInput label="Phone Number" name="phoneNumber" text="0912121212" disabled />
-      </Col>
-      <TitleForm title="Education" />
-      <Col lg={6}>
-        <LabelInput label="University" name="university" text="King Mongkut's University of Technology Thinburi" disabled />
-      </Col>
-      <Col lg={6}>
-        <LabelInput label="Year" name="year" text="3rd" disabled />
-      </Col>
-      <Col lg={6}>
-        <LabelInput label="Major" name="major" text="Information Technology" disabled />
-      </Col>
-      <Col lg={6}>
-        <LabelInput label="GPAX" name="gpax" text="3.00" disabled />
-      </Col>
-      <TitleForm title="Skills" />
-      <Col>
-        {
-          ['UX/U Design', 'Graphic Design', 'Wireframing'].map(tag => (
-            <Tag key={tag}>
-              {tag}
-            </Tag>
-          ))
-        }
-      </Col>
-      <TitleForm title="Experiences" />
-      <Col>
-        <TextArea disabled value={'- Designer at Alchemist, 1 year\n- UX/UI Designer as an outsource, 5 months'} />
-      </Col>
-      <TitleForm title="Documents" />
-      <Col>
-        <Upload
-          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-          listType="picture-card"
-          fileList={[]}
-          onPreview={e => console.log(e)}
-          onChange={e => console.log(e)}
-        >
-          <UploadButton />
-        </Upload>
-        <hr />
-      </Col>
-      <Col className="text-center my-4">
-        <SmallMainButton>
-          <SubTitleSmallWhite className="mb-0">
-            Join Now
-          </SubTitleSmallWhite>
-        </SmallMainButton>
-      </Col>
-    </ContainerRow>
-  </WithNavbar>
+  <WithJoinMatch>
+    <Col>
+      <TitleLarge className="my-2">Junior Programmer Match</TitleLarge>
+    </Col>
+    <TitleForm title="Profile" />
+    <Col lg={6}>
+      <LabelInput label="Firstname" name="firstname" text="Jirapa" disabled />
+    </Col>
+    <Col lg={6}>
+      <LabelInput label="Lastname" name="lastname" text="Songchom" disabled />
+    </Col>
+    <Col lg={6}>
+      <LabelInput label="Email" name="email" text="jirapas.jil@gmail.com" disabled />
+    </Col>
+    <Col lg={6}>
+      <LabelInput label="Phone Number" name="phoneNumber" text="0912121212" disabled />
+    </Col>
+    <TitleForm title="Education" />
+    <Col lg={6}>
+      <LabelInput label="University" name="university" text="King Mongkut's University of Technology Thinburi" disabled />
+    </Col>
+    <Col lg={6}>
+      <LabelInput label="Year" name="year" text="3rd" disabled />
+    </Col>
+    <Col lg={6}>
+      <LabelInput label="Major" name="major" text="Information Technology" disabled />
+    </Col>
+    <Col lg={6}>
+      <LabelInput label="GPAX" name="gpax" text="3.00" disabled />
+    </Col>
+    <TitleForm title="Skills" />
+    <Col>
+      {
+        ['UX/U Design', 'Graphic Design', 'Wireframing'].map(tag => (
+          <Tag key={tag}>
+            {tag}
+          </Tag>
+        ))
+      }
+    </Col>
+    <TitleForm title="Experiences" />
+    <Col>
+      <TextArea disabled value={'- Designer at Alchemist, 1 year\n- UX/UI Designer as an outsource, 5 months'} />
+    </Col>
+    <TitleForm title="Documents" />
+    <Col>
+      <Upload
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        listType="picture-card"
+        fileList={[]}
+        onPreview={e => console.log(e)}
+        onChange={e => console.log(e)}
+      >
+        <UploadButton />
+      </Upload>
+      <hr />
+    </Col>
+    <Col className="text-center my-4">
+      <SmallMainButton>
+        <SubTitleSmallWhite className="mb-0">
+          Join Now
+        </SubTitleSmallWhite>
+      </SmallMainButton>
+    </Col>
+  </WithJoinMatch>
 );
 
 export default ApplicantJoinMatchPage;
