9ec885753656520a92acc73683a6d493c84ef8b8
Keerati <keerati.jear@gmail.com>
2019-09-10 12:06:34 +0700

    [ADD] create tag to base component

---
+++ components/base/Tag.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+  Tag as TagDefault,
+} from 'antd';
+
+const Tag = styled(TagDefault)`
+  
+`;
+
+export default Tag;
