942cdc347b98139b94ad99ea2057b0d71590a22a
Keerati <keerati.jear@gmail.com>
2019-09-19 16:44:00 +0700

    [UPDATE] implement when logged in must show event list page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,32 @@
-import LandingPage from '../components/landing';
+import React from 'react';
+import { connect } from 'react-redux';
 
-export default LandingPage;
+import cookie from '../tools/cookie';
+import withUser from '../tools/with-user';
+
+import LandingPage from '../components/landing';
+import EventListPage from '../components/landing/EventListPage';
+
+class LandingController extends React.Component {
+  static async getInitialProps({
+    store, req, user, ...props
+  }) {
+    const token = cookie.getToken(req);
+    if (req && token) {
+      return { isLoggedIn: true, token };
+    }
+    return { isLoggedIn: false };
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    if (isLoggedIn) {
+      return <EventListPage />;
+    }
+    return <LandingPage />;
+  }
+}
+
+export default withUser(
+  connect()(LandingController),
+);
