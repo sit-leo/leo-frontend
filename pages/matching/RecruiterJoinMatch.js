b2be24265d3157c520ec08be51f2db1d801fd381
Keerati <keerati.jear@gmail.com>
2019-09-24 02:36:12 +0700

    [ADD] fetch match in recruiter join match controller

--- pages/matching/RecruiterJoinMatch.js
+++ pages/matching/RecruiterJoinMatch.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { serverInstance } from '../../tools/request';
+import cookie from '../../tools/cookie';
 import withUser from '../../tools/with-user';
 import { withAuth } from '../../tools/with-auth';
+import redirectToError from '../../tools/redirect-error';
+
+import matchAdapter from '../../store/match/match-adapter';
+import { setMatch } from '../../store/match';
 
 import RecruiterJoinMatchPage from '../../components/matching/RecruiterJoinMatchPage';
 
 class RecruiterJoinMatchController extends React.Component {
-  static async getInitialProps({ store, req }) {
+  static async getInitialProps({
+    store, req, res, query,
+  }) {
+    const { matchId } = query;
+    const matchRequest = matchAdapter(serverInstance(cookie.getToken(req)));
+
+    const match = await matchRequest.getMatchByMatchId(matchId);
+
+    if (match.error) {
+      return redirectToError({ req, res }, 'No Match Found.');
+    }
+
+    await store.dispatch(setMatch(match));
     return {};
   }
 
