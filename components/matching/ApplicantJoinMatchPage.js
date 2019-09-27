9c70350ff04a81bc8059077b16de056c99612583
Keerati <keerati.jear@gmail.com>
2019-09-28 00:14:45 +0700

    [UPDATE] implement applicant profile form in join match page and profile page

--- components/matching/ApplicantJoinMatchPage.js
+++ components/matching/ApplicantJoinMatchPage.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import {
-  Upload, Icon,
-} from 'antd';
 
 import { clientInstance } from '../../tools/request';
 
@@ -11,25 +7,10 @@ import matchingAdapter from '../../store/matching/matching-adapter';
 
 import WithJoinMatch from '../layouts/join-match';
 
-import {
-  setInputSkillVisible as setInputSkillVisibleAction,
-  setSkill as setSkillAction,
-  addApplicantSkill as addApplicantSkillAction,
-  removeApplicantSkill as removeApplicantSkillAction,
-  setExperiences as setExperiencesAction,
-} from '../../store/matching/join';
+import ApplicantProfileForm from '../user/ApplicantProfileForm';
 
 import { Col } from '../base/Grid';
-import { TitleLarge, TitleForm } from '../base/Text';
-import Input, { LabelInput, TextArea } from '../base/Input';
-import Tag from '../base/Tag';
-
-const UploadButton = () => (
-  <div>
-    <Icon type="plus" />
-    <div className="ant-upload-text">Upload</div>
-  </div>
-);
+import { TitleLarge } from '../base/Text';
 
 const handleConfirmApplicant = async (id) => {
   const matchRequest = matchingAdapter(clientInstance());
@@ -38,15 +19,6 @@ const handleConfirmApplicant = async (id) => {
 
 const ApplicantJoinMatchPage = ({
   match,
-  skills = [],
-  addApplicantSkill,
-  removeApplicantSkill,
-  skill,
-  setSkill,
-  inputSkillVisible,
-  setInputSkillVisible,
-  experiences,
-  setExperiences,
 }) => (
   <WithJoinMatch
     handleConfirm={() => handleConfirmApplicant(match.id)}
@@ -54,109 +26,12 @@ const ApplicantJoinMatchPage = ({
     <Col>
       <TitleLarge className="my-2">{match.name}</TitleLarge>
     </Col>
-    <TitleForm title="Profile" />
-    <Col lg={6}>
-      <LabelInput label="Firstname" name="firstname" text="Jirapa" disabled />
-    </Col>
-    <Col lg={6}>
-      <LabelInput label="Lastname" name="lastname" text="Songchom" disabled />
-    </Col>
-    <Col lg={6}>
-      <LabelInput label="Email" name="email" text="jirapas.jil@gmail.com" disabled />
-    </Col>
-    <Col lg={6}>
-      <LabelInput label="Phone Number" name="phoneNumber" text="0912121212" disabled />
-    </Col>
-    <TitleForm title="Education" />
-    <Col lg={6}>
-      <LabelInput label="University" name="university" text="King Mongkut's University of Technology Thinburi" disabled />
-    </Col>
-    <Col lg={6}>
-      <LabelInput label="Year" name="year" text="3rd" disabled />
-    </Col>
-    <Col lg={6}>
-      <LabelInput label="Major" name="major" text="Information Technology" disabled />
-    </Col>
-    <Col lg={6}>
-      <LabelInput label="GPAX" name="gpax" text="3.00" disabled />
-    </Col>
-    <TitleForm title="Skills" />
-    <Col>
-      {
-        skills.map(tag => (
-          <Tag
-            closable
-            key={`${tag}-${Math.random()}`}
-            onClose={(e) => {
-              e.preventDefault();
-              removeApplicantSkill(tag);
-            }}
-          >
-            {tag}
-          </Tag>
-        ))
-      }
-      {
-        inputSkillVisible && (
-          <Input
-            type="text"
-            size="small"
-            style={{ width: 78 }}
-            value={skill}
-            onChange={e => setSkill(e.target.value)}
-            onBlur={() => setInputSkillVisible(false)}
-            onPressEnter={() => {
-              if (skill !== '') {
-                addApplicantSkill(skill);
-                setSkill('');
-              }
-            }}
-          />
-        )
-      }
-      {
-        !inputSkillVisible && (
-          <Tag onClick={() => setInputSkillVisible(!inputSkillVisible)}>
-            <Icon type="plus" />
-            Add Skill
-          </Tag>
-        )
-      }
-    </Col>
-    <TitleForm title="Experiences" />
-    <Col>
-      <TextArea rows={3} value={experiences} onChange={e => setExperiences(e.target.value)} />
-    </Col>
-    <TitleForm title="Documents" />
-    <Col>
-      <Upload
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        listType="picture-card"
-        fileList={[]}
-        onPreview={e => console.log(e)}
-        onChange={e => console.log(e)}
-      >
-        <UploadButton />
-      </Upload>
-      <hr />
-    </Col>
+    <ApplicantProfileForm />
   </WithJoinMatch>
 );
 
 const mapStateToProps = state => ({
   match: state.match.match,
-  inputSkillVisible: state.join.inputSkillVisible,
-  skills: state.join.applicant.skills,
-  experiences: state.join.applicant.experiences,
-  skill: state.join.skill,
 });
 
-const mapDispatchToProps = dispatch => ({
-  addApplicantSkill: bindActionCreators(addApplicantSkillAction, dispatch),
-  removeApplicantSkill: bindActionCreators(removeApplicantSkillAction, dispatch),
-  setInputSkillVisible: bindActionCreators(setInputSkillVisibleAction, dispatch),
-  setSkill: bindActionCreators(setSkillAction, dispatch),
-  setExperiences: bindActionCreators(setExperiencesAction, dispatch),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ApplicantJoinMatchPage);
+export default connect(mapStateToProps)(ApplicantJoinMatchPage);
--- components/user/ProfilePage.js
+++ components/user/ProfilePage.js
@@ -1,47 +1,90 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import {
+  Upload,
+} from 'antd';
 
 import { clientInstance } from '../../tools/request';
 
 import userAdapter from '../../store/user/user-adapter';
-// import {} from '../../store/user';
 
 import WithNavbar from '../layouts/with-navbar';
 
 import ContainerRow, { Col } from '../base/Grid';
-import Form, { Item } from '../base/Form';
-import Input from '../base/Input';
+import Form, { FormContainer } from '../base/Form';
+import {
+  TitleForm, SubTitleWhite, NoteText,
+} from '../base/Text';
+import Icon from '../base/Icon';
+import ApplicantProfileForm from './ApplicantProfileForm';
+import { SmallMainButton } from '../base/Button';
+import { LabelInput } from '../base/Input';
 
 const userRequest = userAdapter(clientInstance());
+
+const UploadButton = () => (
+  <div>
+    <Icon type="plus" />
+    <div className="ant-upload-text">Upload</div>
+  </div>
+);
 
 const LoginPage = ({
   form: { getFieldDecorator, validateFields },
 }) => (
   <WithNavbar>
     <ContainerRow>
-      <Col lg={{ size: 6, offset: 3 }}>
-        {/* <Form
-          method="POST"
-          className="w-100 py-4 px-4 card"
-          onSubmit={(e) => {
-            e.preventDefault();
-            validateFields((err, values) => {
-              if (!err) {
-                console.log(values);
-              }
-            });
-          }}
-        >
-
-        </Form> */}
-      </Col>
+      <FormContainer
+        method="POST"
+        className="w-100 py-4 px-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+          validateFields((err, values) => {
+            if (!err) {
+              console.log(values);
+            }
+          });
+        }}
+      >
+        <ApplicantProfileForm />
+        <TitleForm title="Documents" />
+        <Col>
+          <Upload
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            listType="picture-card"
+            fileList={[]}
+            onPreview={e => console.log(e)}
+            onChange={e => console.log(e)}
+          >
+            <UploadButton />
+          </Upload>
+          <hr />
+        </Col>
+        <TitleForm title="Change password" />
+        <Col lg={6}>
+          <LabelInput label="Current password" />
+        </Col>
+        <Col lg={6}>
+          <LabelInput label="New password" />
+        </Col>
+        <Col>
+          <NoteText>Leave it blank if you don’t want to change it.</NoteText>
+        </Col>
+        <Col className="text-center my-4">
+          <hr />
+          <SmallMainButton>
+            <SubTitleWhite className="mb-0">
+              Update Profile
+            </SubTitleWhite>
+          </SmallMainButton>
+        </Col>
+      </FormContainer>
     </ContainerRow>
   </WithNavbar>
 );
 
 const mapStateToProps = state => ({
-  // profile: state.user.profile,
+  // profile: state.profile,
 });
 
 const mapDispatchToProps = dispatch => ({
