b381194ee1003c96ff14f58e93e667fb7bd2fc79
Keerati <keerati.jear@gmail.com>
2019-09-28 02:23:16 +0700

    [UPDATE] implement setter function for applicant profile to redux

--- components/user/ApplicantProfileForm.js
+++ components/user/ApplicantProfileForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { Label } from 'reactstrap';
 
 import {
   setInputSkillVisible as setInputSkillVisibleAction,
@@ -8,53 +9,159 @@ import {
   addApplicantSkill as addApplicantSkillAction,
   removeApplicantSkill as removeApplicantSkillAction,
   setExperiences as setExperiencesAction,
+  setFirstname as setFirstnameAction,
+  setLastname as setLastnameAction,
+  setEmail as setEmailAction,
+  setTelno as setTelnoAction,
+  setEducation as setEducationAction,
 } from '../../store/user/profile';
 
 import { TitleForm } from '../base/Text';
-import Input, { LabelInput, TextArea } from '../base/Input';
+import Input, {
+  InputNumber,
+  LabelInput,
+  TextArea,
+} from '../base/Input';
 import Tag from '../base/Tag';
 import Icon from '../base/Icon';
 import { Col } from '../base/Grid';
+import { Item } from '../base/Form';
 
 const ApplicantProfileForm = ({
   editable,
-  skills = [],
+  getFieldDecorator,
+  skill,
+  applicant: {
+    firstName,
+    lastName,
+    email,
+    telNo,
+    educations,
+    skills = [],
+    experiences,
+  },
+  inputSkillVisible,
+  setSkill,
   addApplicantSkill,
   removeApplicantSkill,
-  skill,
-  setSkill,
-  inputSkillVisible,
-  setInputSkillVisible,
-  experiences,
   setExperiences,
+  setInputSkillVisible,
+  setFirstname,
+  setLastname,
+  setEmail,
+  setTelno,
+  setEducation,
 }) => (
   <React.Fragment>
     <TitleForm title="Profile" />
     <Col lg={6}>
-      <LabelInput label="Firstname" name="firstname" text="Jirapa" disabled />
+      <LabelInput
+        label="Firstname"
+        name="firstname"
+        text={firstName}
+        disabled={!editable}
+        getFieldDecorator={getFieldDecorator}
+        onChange={e => setFirstname(e.target.value)}
+      />
     </Col>
     <Col lg={6}>
-      <LabelInput label="Lastname" name="lastname" text="Songchom" disabled />
+      <LabelInput
+        label="Lastname"
+        name="lastname"
+        text={lastName}
+        disabled={!editable}
+        getFieldDecorator={getFieldDecorator}
+        onChange={e => setLastname(e.target.value)}
+      />
     </Col>
     <Col lg={6}>
-      <LabelInput label="Email" name="email" text="jirapas.jil@gmail.com" disabled />
+      <LabelInput
+        label="Email"
+        name="email"
+        text={email}
+        disabled={!editable}
+        getFieldDecorator={getFieldDecorator}
+        onChange={e => setEmail(e.target.value)}
+      />
     </Col>
     <Col lg={6}>
-      <LabelInput label="Phone Number" name="phoneNumber" text="0912121212" disabled />
+      <LabelInput
+        label="Phone Number"
+        name="phoneNumber"
+        text={telNo}
+        disabled={!editable}
+        getFieldDecorator={getFieldDecorator}
+        onChange={e => setTelno(e.target.value)}
+      />
     </Col>
     <TitleForm title="Education" />
-    <Col lg={6}>
-      <LabelInput label="University" name="university" text="King Mongkut's University of Technology Thinburi" disabled />
-    </Col>
-    <Col lg={6}>
-      <LabelInput label="Year" name="year" text="3rd" disabled />
-    </Col>
-    <Col lg={6}>
-      <LabelInput label="Major" name="major" text="Information Technology" disabled />
-    </Col>
-    <Col lg={6}>
-      <LabelInput label="GPAX" name="gpax" text="3.00" disabled />
-    </Col>
+    {
+      educations.map(({
+        university,
+        year,
+        major,
+        gpax,
+      }) => (
+        <React.Fragment key={`${university}-education`}>
+          <Col lg={6}>
+            <LabelInput
+              label="University"
+              name="university"
+              text={university}
+              disabled={!editable}
+              getFieldDecorator={getFieldDecorator}
+              onChange={e => setEducation('university', e.target.value)}
+            />
+          </Col>
+          <Col lg={6}>
+            <LabelInput
+              label="Year"
+              name="year"
+              text={year}
+              disabled={!editable}
+              getFieldDecorator={getFieldDecorator}
+              onChange={e => setEducation('year', e.target.value)}
+            />
+          </Col>
+          <Col lg={6}>
+            <LabelInput
+              label="Major"
+              name="major"
+              text={major}
+              disabled={!editable}
+              getFieldDecorator={getFieldDecorator}
+              onChange={e => setEducation('major', e.target.value)}
+            />
+          </Col>
+          <Col lg={6}>
+            <Label for="gpax" className="mb-0">GPAX</Label>
+            <Item>
+              {
+                getFieldDecorator('gpax', {
+                  validateTrigger: ['onBlur'],
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please fill "GPAX".',
+                      setFieldsValue: gpax,
+                    },
+                  ],
+                })(
+                  <InputNumber
+                    min="1.00"
+                    max="4.00"
+                    step="0.01"
+                    disabled={!editable}
+                    onChange={value => setEducation('gpax', value)}
+                    className="w-100"
+                  />,
+                )
+              }
+            </Item>
+          </Col>
+        </React.Fragment>
+      ))
+    }
     <TitleForm title="Skills" />
     <Col>
       {
@@ -107,9 +214,8 @@ const ApplicantProfileForm = ({
 
 const mapStateToProps = state => ({
   inputSkillVisible: state.profile.inputSkillVisible,
-  skills: state.profile.applicant.skills,
-  experiences: state.profile.applicant.experiences,
   skill: state.profile.skill,
+  applicant: state.profile.applicant,
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -118,6 +224,11 @@ const mapDispatchToProps = dispatch => ({
   setInputSkillVisible: bindActionCreators(setInputSkillVisibleAction, dispatch),
   setSkill: bindActionCreators(setSkillAction, dispatch),
   setExperiences: bindActionCreators(setExperiencesAction, dispatch),
+  setFirstname: bindActionCreators(setFirstnameAction, dispatch),
+  setLastname: bindActionCreators(setLastnameAction, dispatch),
+  setEmail: bindActionCreators(setEmailAction, dispatch),
+  setTelno: bindActionCreators(setTelnoAction, dispatch),
+  setEducation: bindActionCreators(setEducationAction, dispatch),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ApplicantProfileForm);
