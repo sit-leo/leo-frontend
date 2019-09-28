510b297ac6761e97f81d1b41bfba886015554bdb
Keerati <keerati.jear@gmail.com>
2019-09-28 23:45:06 +0700

    [ADD] implement set profile with role

--- pages/profile/Profile.js
+++ pages/profile/Profile.js
@@ -5,18 +5,28 @@ import { withAuth } from '../../tools/with-auth';
 import withUser from '../../tools/with-user';
 import { serverInstance } from '../../tools/request';
 import cookie from '../../tools/cookie';
+import { isApplicant, isRecruiter } from '../../tools/with-roles';
 
 import profileAdapter from '../../store/profile/profile-adapter';
+import userAdapter from '../../store/user/user-adapter';
 
 import ProfilePage from '../../components/profile/ProfilePage';
 
-import { setApplicantProfile, setEducation } from '../../store/profile';
+import { setApplicantProfile, setEducation, setRecruiterProfile } from '../../store/profile';
 
 class ProfileController extends React.Component {
   static async getInitialProps({
     store, req, res, query,
   }) {
+    const userRequest = userAdapter(serverInstance(cookie.getToken(req)));
     const profileRequest = profileAdapter(serverInstance(cookie.getToken(req)));
+
+    const user = await userRequest.getUser();
+
+    if (user.error) {
+      // redirect error or to login page.
+      return {};
+    }
 
     const profile = await profileRequest.getProfile();
 
@@ -25,8 +35,15 @@ class ProfileController extends React.Component {
       return {};
     }
 
-    store.dispatch(setApplicantProfile(profile));
-    store.dispatch(setEducation('applicantId', profile.applicantId));
+    if (isApplicant(user.role)) {
+      store.dispatch(setApplicantProfile(profile));
+      store.dispatch(setEducation('applicantId', profile.applicantId));
+    }
+
+    if (isRecruiter(user.role)) {
+      store.dispatch(setRecruiterProfile(profile));
+    }
+
 
     return {};
   }
