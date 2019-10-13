6e7f5a4f65591aefe4a77955918732eb721a8570
Keerati <keerati.jear@gmail.com>
2019-10-14 04:11:30 +0700

    [UPDATE] implement select document with file from profile

--- components/matching/UploadStep.js
+++ components/matching/UploadStep.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-import { TitleSmall } from '../base/Text';
+import RankingCard from './RankingCard';
 
-const Upload = () => (
-  <TitleSmall>Upload Document</TitleSmall>
+const Upload = ({
+  applicantRanks,
+  files,
+}) => (
+  <React.Fragment>
+    {
+      applicantRanks.map(({ position }, index) => (
+        <RankingCard
+          key={position.id}
+          title={position.name}
+          value={position.money}
+          subtitle={(position.recruiter && `${position.recruiter.name}, ${position.recruiter.location}`) || '-'}
+          capacity={position.capacity}
+          files={files}
+          actionButton={null}
+        />
+      ))
+    }
+  </React.Fragment>
 );
 
-export default Upload;
+const mapStateToProps = state => ({
+  match: state.match.match,
+  applicantRanks: state.ranking.applicantRanks,
+  files: state.profile.files,
+});
+
+const mapDispatchToRankProps = dispatch => ({
+  // updateRank: bindActionCreators(updateApplicantRank, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToRankProps)(Upload);
