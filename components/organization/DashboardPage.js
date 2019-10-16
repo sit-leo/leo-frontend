c0218eba1e4d98abafd220176c6f83388cbc0fe4
Keerati <keerati.jear@gmail.com>
2019-10-16 18:09:43 +0700

    [UPDATE] add statistic header to dashboard

--- components/organization/DashboardPage.js
+++ components/organization/DashboardPage.js
@@ -1,9 +1,78 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import Organization from '../layouts/organization';
+
+import { Col, Row } from '../base/Grid';
+import {
+  TitleLargePrimary,
+  TitleLargeWhite,
+  SubTitleStatistic,
+} from '../base/Text';
+import Card from '../base/Card';
+import { IconLargeWhite } from '../base/Icon';
+import color from '../../config/color';
+
+const StatisticCard = styled.a`
+  padding: 8px 16px;
+  margin: 10px 0;
+  max-height: 89px;
+  border-radius: 4px;
+  box-shadow: 0 2px 10px 0 rgba(96, 84, 84, 0.1);
+  text-decoration: none !important;
+  cursor: pointer;
+  transition: .15s;
+  background-color: ${props => props.color || color.hover};
+
+  &:hover {
+    box-shadow: 0 5px 10px 0 rgba(23, 23, 23, 0.25);
+  }
+
+  @media (min-width: 1366px) {
+    max-width: 255px;
+  }
+`;
+
+const Statistic = ({
+  number,
+  text,
+  url,
+  cardColor,
+}) => (
+  <StatisticCard href={url} className="d-block w-100" color={cardColor}>
+    <Row>
+      <Col xs={9}>
+        <TitleLargeWhite className="mb-0">
+          {number}
+        </TitleLargeWhite>
+        <SubTitleStatistic>
+          {text}
+        </SubTitleStatistic>
+      </Col>
+      <Col xs={3} className="d-flex align-items-center justify-content-center">
+        <IconLargeWhite type="right-circle" />
+      </Col>
+    </Row>
+  </StatisticCard>
+);
 
 const DashboardPage = () => (
-  <div>
-    Dashboard Page
-  </div>
+  <Organization title="About Organization">
+    <Col className="d-lg-flex justify-content-between">
+      <Statistic url="#" number={5} text="Matches" cardColor="#58b0ad" />
+      <Statistic url="/organizations/recruiters/add" number={19} text="Recruiters" cardColor="#58b09e" />
+      <Statistic url="/organizations/applicants/add" number={439} text="Applicants" cardColor="#58b090" />
+      <Statistic url="/organizations/matches/create" number="Create Now" text="Current Matches" cardColor="#58b081" />
+    </Col>
+    <Col>
+      <Card className="my-3">
+        <TitleLargePrimary>
+          Statistics
+          <hr />
+        </TitleLargePrimary>
+      </Card>
+    </Col>
+  </Organization>
 );
 
 export default DashboardPage;
