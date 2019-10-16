b1ffe470cd64691997684b2837b9c4aeed4bb19e
Keerati <keerati.jear@gmail.com>
2019-10-16 17:08:40 +0700

    [ADD] create add member container

---
+++ components/organization/AddMemberContainer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Table } from 'antd';
+
+import Organization from '../layouts/organization';
+
+import { Search } from '../base/Input';
+import ContainerRow, { Col } from '../base/Grid';
+
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+  getCheckboxProps: record => ({
+    disabled: record.name === 'Disabled User', // Column configuration not to be checked
+    name: record.name,
+  }),
+};
+
+const AddMemberContainer = ({
+  title,
+  columns,
+  dataSource,
+}) => (
+  <Organization title={title}>
+    <ContainerRow>
+      <Col md={{ size: 6, offset: 3 }}>
+        <Search />
+      </Col>
+      <Col className="my-4" md={{ size: 10, offset: 1 }}>
+        <Table
+          rowSelection={rowSelection}
+          columns={columns}
+          dataSource={dataSource}
+        />
+      </Col>
+    </ContainerRow>
+  </Organization>
+);
+
+export default AddMemberContainer;
