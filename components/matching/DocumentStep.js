49871c86a7bef4cbcc9cb3beb987d6691555a936
Keerati <keerati.jear@gmail.com>
2019-10-17 17:34:02 +0700

    [ADD] create alert , show sequence and files in position

--- components/matching/DocumentStep.js
+++ components/matching/DocumentStep.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Alert } from 'antd';
 
 import RankingCard from './RankingCard';
 
 const DocumentStep = ({
   applicantRanks,
-  files,
 }) => (
   <React.Fragment>
+    <Alert
+      message="Information"
+      description='Please remove "unused" or "not required" document in "Show more" section before clicking "Confirm Button". Document will be uploaded to the recruiter of positions.'
+      type="info"
+      showIcon
+    />
     {
-      applicantRanks.map(({ position }, index) => (
+      applicantRanks.map(({ position, sequence, files }, index) => (
         <RankingCard
+          sequence={sequence}
           key={position.id}
           title={position.name}
           value={position.money}
           subtitle={(position.recruiter && `${position.recruiter.name}, ${position.recruiter.location}`) || '-'}
           capacity={position.capacity}
+          informations={[
+            {
+              header: 'Required Documents',
+              detail: 'Resume, Transcript',
+            },
+          ]}
           files={files}
           actionButton={null}
         />
@@ -27,7 +40,6 @@ const DocumentStep = ({
 const mapStateToProps = state => ({
   match: state.match.match,
   applicantRanks: state.ranking.applicantRanks,
-  files: state.profile.files,
 });
 
 const mapDispatchToRankProps = dispatch => ({
