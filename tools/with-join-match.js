8446f13bc7a6c02e8a0c58092c67afbab952da38
Keerati <keerati.jear@gmail.com>
2019-10-29 00:43:24 +0700

    [ADD] create hoc for check is join match

---
+++ tools/with-join-match.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { serverInstance } from './request';
+
+import matchingAdapter from '../store/matching/matching-adapter';
+import redirectToLogin from './reditect-login';
+import redirectToError from './redirect-error';
+
+const withJoinMatch = WrappedComponent => class extends Component {
+  static async getInitialProps(ctx) {
+    const props = WrappedComponent.getInitialProps
+          && (await WrappedComponent.getInitialProps(ctx));
+
+    const matchingRequest = matchingAdapter(serverInstance(props.token));
+
+    const { matchId } = ctx.query;
+    const isJoined = await matchingRequest.isJoined(matchId);
+
+    if (isJoined.error) {
+      redirectToLogin(ctx);
+      return {};
+    }
+
+    if (!isJoined.joined) {
+      redirectToError(ctx, "You're not joining this match.");
+      return {};
+    }
+
+    return { ...props };
+  }
+
+  render() {
+    return <WrappedComponent {...this.props} />;
+  }
+};
+
+export default withJoinMatch;
