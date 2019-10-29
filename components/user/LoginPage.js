0069f5f9bd0563f2e7640d1dfe9d5967c44b046c
Keerati <keerati.jear@gmail.com>
2019-10-29 11:16:36 +0700

    [UPDATE] use loading hoc for login page

--- components/user/LoginPage.js
+++ components/user/LoginPage.js
@@ -9,8 +9,8 @@ import userAdapter from '../../store/user/user-adapter';
 import { setUsername, setPassword } from '../../store/user';
 
 import WithNavbar from '../layouts/with-navbar';
+import WithLoading from '../layouts/with-loading';
 
-import Loading from '../base/Loading';
 import { SmallMainButton } from '../base/Button';
 import Input from '../base/Input';
 import ContainerRow, { Col } from '../base/Grid';
@@ -31,97 +31,97 @@ const LoginPage = ({
 }) => {
   const [error, setError] = useState(null);
   return (
-    <WithNavbar>
-      <Loading loading={loading} />
-      <FlexCenter className="vh-100">
-        <ContainerRow>
-          <Col lg={{ size: 6, offset: 3 }}>
-            <Form
-              method="POST"
-              className="w-100 py-4 px-4 card"
-              onSubmit={(e) => {
-                e.preventDefault();
-                validateFields((err, values) => {
-                  if (!err) {
-                    setLoading(true);
-                    userRequest.login({ username, password }).then(({ status }) => {
-                      if (status && status !== 200) {
-                        setError('You have entered wrong password. Please try again.');
-                        setLoading(false);
-                      } else {
-                        setError(null);
-                        message.success('Login success.');
-                        window.location.assign('/');
-                      }
-                    });
-                  }
-                });
-              }}
-            >
-              <Col className="text-center mb-3">
-                <img className="w-25" src="/static/images/leo.png" alt="LEO-Logo" />
-              </Col>
-              {
-                error && (
-                  <Col className="px-0 pb-2">
-                    <Alert
-                      message="Error"
-                      description={error}
-                      type="error"
-                      showIcon
-                    />
-                  </Col>
-                )
-              }
-              <Item>
-                {getFieldDecorator('email', {
-                  validateTrigger: ['onBlur'],
-                  rules: [
-                    {
-                      required: true,
-                      message: 'Please fill your email.',
-                      setFieldsValue: username,
-                    },
-                  ],
-                })(
-                  <Input
-                    onChange={e => handleUsername(e.target.value)}
-                    placeholder="Email"
-                  />,
-                )}
-              </Item>
+    <WithLoading>
+      <WithNavbar>
+        <FlexCenter className="vh-100">
+          <ContainerRow>
+            <Col lg={{ size: 6, offset: 3 }}>
+              <Form
+                method="POST"
+                className="w-100 py-4 px-4 card"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  validateFields((err, values) => {
+                    if (!err) {
+                      setLoading(true);
+                      userRequest.login({ username, password }).then(({ status }) => {
+                        if (status && status !== 200) {
+                          setError('You have entered wrong password. Please try again.');
+                          setLoading(false);
+                        } else {
+                          setError(null);
+                          message.success('Login success.');
+                          window.location.assign('/');
+                        }
+                      });
+                    }
+                  });
+                }}
+              >
+                <Col className="text-center mb-3">
+                  <img className="w-25" src="/static/images/leo.png" alt="LEO-Logo" />
+                </Col>
+                {
+                  error && (
+                    <Col className="px-0 pb-2">
+                      <Alert
+                        message="Error"
+                        description={error}
+                        type="error"
+                        showIcon
+                      />
+                    </Col>
+                  )
+                }
+                <Item>
+                  {getFieldDecorator('email', {
+                    validateTrigger: ['onBlur'],
+                    rules: [
+                      {
+                        required: true,
+                        message: 'Please fill your email.',
+                        setFieldsValue: username,
+                      },
+                    ],
+                  })(
+                    <Input
+                      onChange={e => handleUsername(e.target.value)}
+                      placeholder="Email"
+                    />,
+                  )}
+                </Item>
 
-              <Item>
-                {getFieldDecorator('password', {
-                  validateTrigger: ['onBlur'],
-                  rules: [
-                    {
-                      required: true,
-                      message: 'Please fill your password.',
-                      setFieldsValue: password,
-                    },
-                  ],
-                })(
-                  <Input
-                    type="password"
-                    onChange={e => handlePassword(e.target.value)}
-                    placeholder="password"
-                  />,
-                )}
-              </Item>
-              <SmallMainButton htmlType="submit" className="my-3 w-25 mx-auto">Sign in</SmallMainButton>
-            </Form>
-          </Col>
-        </ContainerRow>
-      </FlexCenter>
-    </WithNavbar>
+                <Item>
+                  {getFieldDecorator('password', {
+                    validateTrigger: ['onBlur'],
+                    rules: [
+                      {
+                        required: true,
+                        message: 'Please fill your password.',
+                        setFieldsValue: password,
+                      },
+                    ],
+                  })(
+                    <Input
+                      type="password"
+                      onChange={e => handlePassword(e.target.value)}
+                      placeholder="password"
+                    />,
+                  )}
+                </Item>
+                <SmallMainButton htmlType="submit" className="my-3 w-25 mx-auto">Sign in</SmallMainButton>
+              </Form>
+            </Col>
+          </ContainerRow>
+        </FlexCenter>
+      </WithNavbar>
+    </WithLoading>
   );
 };
 
 const mapStateToProps = state => ({
   username: state.user.username,
   password: state.user.password,
-  loading: state.global.loading,
 });
 
 const mapDispatchToProps = dispatch => ({
