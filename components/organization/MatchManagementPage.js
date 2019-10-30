bdd8b86bf065d646a08078fb49b9b0d1b9bb0026
Keerati <keerati.jear@gmail.com>
2019-10-30 18:36:48 +0700

    [ADD] create match form page

--- components/organization/MatchManagementPage.js
+++ components/organization/MatchManagementPage.js
@@ -1,9 +1,159 @@
 import React from 'react';
+import { Label } from 'reactstrap';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-const MatchManagementPage = () => (
-  <div>
-    Match Management Page
-  </div>
+import Organization from '../layouts/organization';
+
+import { setMatchValueByAttribute as setMatchValueByAttributeAction } from '../../store/match';
+
+import { Col } from '../base/Grid';
+import Form, { FormContainer } from '../base/Form';
+import { TitleMedium, SubTitleWhite, TitleForm } from '../base/Text';
+import { SmallMainButton } from '../base/Button';
+import {
+  LabelInput, TextArea, DateRangePicker, DatePicker,
+} from '../base/Input';
+
+import CoverImage from './CoverImage';
+
+function isCreateOrUpdate(haveCurrentMatch) {
+  return !haveCurrentMatch ? 'Create Match' : 'Update Match';
+}
+
+const MatchManagementPage = ({
+  isCurrentMatch = false,
+  match: {
+    name,
+    description,
+    startJoiningDate,
+    endJoiningDate,
+    endApplicantRankingDate,
+    endRecruiterRankingDate,
+    announceDate,
+  },
+  form: { getFieldDecorator, setFieldsValue, validateFields },
+  setMatchValue = () => { },
+}) => (
+  <Organization title={isCreateOrUpdate(isCurrentMatch)}>
+    <FormContainer
+      onSubmit={(e) => {
+        e.preventDefault();
+        validateFields((err, values) => {
+          console.log(values);
+          if (!err) {
+            // createMatch();
+            // updateMatch();
+          }
+        });
+      }}
+    >
+      <Col className="text-center">
+        <hr />
+        <CoverImage />
+        <TitleMedium className="my-2 text-center">
+          <b>Cover Photo</b>
+        </TitleMedium>
+      </Col>
+
+      <TitleForm title="Match detail" />
+      <Col>
+        <LabelInput
+          label="Match name"
+          name="name"
+          text={name}
+          onChange={e => setMatchValue('name', e.target.value)}
+          getFieldDecorator={getFieldDecorator}
+        />
+      </Col>
+      <Col>
+        <Label for="description" className="mb-0">Description</Label>
+        {
+          getFieldDecorator('description', {
+            initialValue: description,
+            rules: [
+              {
+                required: true,
+                message: 'Please fill "Description".',
+              },
+            ],
+          })(
+            <TextArea onChange={e => setMatchValue('description', e.target.value)} />,
+          )
+        }
+      </Col>
+      <TitleForm title="Date detail" />
+      <Col lg={6} className="my-2">
+        <DateRangePicker
+          label="Join Period"
+          name="joinPeriod"
+          getFieldDecorator={getFieldDecorator}
+          value={{ startJoiningDate, endJoiningDate }}
+          onChange={(joinPeriod) => {
+            const [startDate, endDate] = joinPeriod;
+            setMatchValue('startJoiningDate', startDate);
+            setMatchValue('endJoiningDate', endDate);
+            setFieldsValue({ joinPeriod });
+          }}
+        />
+      </Col>
+      <Col lg={6} className="my-2">
+        <DatePicker
+          label="Applicant Ranking End Date"
+          name="endApplicantRankingDate"
+          value={announceDate}
+          onChange={(date) => {
+            setMatchValue('endApplicantRankingDate', date.format());
+            setFieldsValue({ endApplicantRankingDate: date });
+          }}
+          getFieldDecorator={getFieldDecorator}
+        />
+      </Col>
+      <Col lg={6} className="my-2">
+        <DatePicker
+          label="Recruiter Ranking End Date"
+          name="endRecruiterRankingDate"
+          value={announceDate}
+          onChange={(date) => {
+            setMatchValue('endRecruiterRankingDate', date.format());
+            setFieldsValue({ endRecruiterRankingDate: date });
+          }}
+          getFieldDecorator={getFieldDecorator}
+        />
+      </Col>
+      <Col lg={6} className="my-2">
+        <DatePicker
+          label="Announce Date"
+          name="announceDate"
+          value={announceDate}
+          onChange={(date) => {
+            setMatchValue('announceDate', date.format());
+            setFieldsValue({ announceDate: date });
+          }}
+          getFieldDecorator={getFieldDecorator}
+        />
+      </Col>
+
+      <Col className="text-center">
+        <hr />
+        <SmallMainButton htmlType="submit" className="mt-3 mb-5">
+          <SubTitleWhite className="mb-0">
+            {isCreateOrUpdate(isCurrentMatch)}
+          </SubTitleWhite>
+        </SmallMainButton>
+      </Col>
+    </FormContainer>
+  </Organization>
 );
 
-export default MatchManagementPage;
+const mapStateToProps = state => ({
+  match: state.match.match,
+});
+
+const mapDispatchToProps = dispatch => ({
+  setMatchValue: bindActionCreators(setMatchValueByAttributeAction, dispatch),
+});
+
+const WrappedMatchManagementPage = Form.create({ name: 'match_management_page' })(MatchManagementPage);
+
+export default connect(mapStateToProps, mapDispatchToProps)(WrappedMatchManagementPage);
