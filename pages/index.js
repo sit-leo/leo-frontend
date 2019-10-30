b4d1687b8abe128ff26d90e27874886e73c078c0
Keerati <keerati.jear@gmail.com>
2019-10-30 23:06:11 +0700

    [UPDATE] replace landing  redirect to matches page if logged in

--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import cookie from '../tools/cookie';
-import withUser from '../tools/with-user';
 
 import LandingPage from '../components/landing';
-import EventListPage from '../components/landing/EventListPage';
 
 class LandingController extends React.Component {
   static async getInitialProps({
-    store, req, user, ...props
+    store, req, res,
   }) {
     const token = cookie.getToken(req);
     if (req && token) {
-      return { isLoggedIn: true, token };
+      res.writeHead(302, { Location: '/matches' });
+      res.end();
+      return { token };
     }
-    return { isLoggedIn: false };
+    return {};
   }
 
   render() {
-    const { isLoggedIn } = this.props;
-    if (isLoggedIn) {
-      return <EventListPage />;
-    }
     return <LandingPage />;
   }
 }
 
-export default withUser(
-  connect()(LandingController),
-);
+export default connect()(LandingController);
