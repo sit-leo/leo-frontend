49f7e7330824a741cfe6e2809db75834aa0582c8
Keerati <keerati.jear@gmail.com>
2019-10-31 01:25:29 +0700

    [UPDATE] implement check organizer in organization apge

--- pages/organization/index.js
+++ pages/organization/index.js
@@ -2,31 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { withAuth } from '../../tools/with-auth';
+import { withOrganizer } from '../../tools/with-roles';
 import withUser from '../../tools/with-user';
-import { isOrganizer } from '../../tools/with-roles';
+
+
+import MyOrganizationsPage from '../../components/organization/MyOrganizationsPage';
 
 class OrganizationController extends React.Component {
   static async getInitialProps({
     store, query, req, res,
   }) {
-    const role = 'organizer';
-
-    if (isOrganizer(role)) {
-      res.writeHead(302, { Location: '/organizations/dashboard' });
-      res.end();
-    }
-
     return {};
   }
 
   render() {
-    return <div>Orgainzations Controller</div>;
+    return <MyOrganizationsPage />;
   }
 }
 
 
-export default withUser(
-  withAuth(
-    connect()(OrganizationController),
+export default withOrganizer(
+  withUser(
+    withAuth(
+      connect()(OrganizationController),
+    ),
   ),
 );
