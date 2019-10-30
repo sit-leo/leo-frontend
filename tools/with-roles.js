46e17891dc2bd667fb5d2a7c9ee8700ba57b4a80
Keerati <keerati.jear@gmail.com>
2019-10-31 00:03:54 +0700

    [ADD] create hoc for check organizer

--- tools/with-roles.js
+++ tools/with-roles.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { serverInstance } from './request';
+
+import userAdapter from '../store/user/user-adapter';
+import cookie from './cookie';
+
 export const ROLE_APPLICANT = 'applicant';
 export const ROLE_RECRUITER = 'recruiter';
 export const ROLE_ORGANIZER = 'organizer';
@@ -46,3 +51,35 @@ const withRole = checkRole => WrappedComponent => connect()(
 );
 
 export default withRole;
+
+/* eslint-disable */
+export const withOrganizer = WrappedComponent => connect()(
+  class extends Component {
+    static async getInitialProps(ctx) {
+      const props = WrappedComponent.getInitialProps
+        && (await WrappedComponent.getInitialProps(ctx));
+
+      const token = cookie.getToken(ctx.req);
+
+      if (token) {
+        if (!user.error && isOrganizer(user.role)) {
+          const userRequest = userAdapter(serverInstance(token));
+          const user = await userRequest.getUser();
+          switch (ctx.req.url) {
+            case '/organizations':
+            default: {
+              ctx.res.writeHead(302, { Location: '/organizations/dashboard' });
+            }
+          }
+          ctx.res.end();
+        }
+      }
+
+      return { ...props };
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  },
+);
