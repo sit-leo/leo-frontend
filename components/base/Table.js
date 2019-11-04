cf30c6246132cada4570dfc6c27bf82938e7ba36
jirapa songchom <jirapa.songchom@mail.kmutt.ac.th>
2019-11-05 00:50:25 +0700

    [ADD] Create file base Table to apply style

---
+++ components/base/Table.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Table as DefaultTable } from 'antd';
+
+const Table = styled(DefaultTable)`
+    .ant-table table{
+      border-collapse: collapse;
+      background-color: #ffffff;
+    }
+`;
+
+export default Table;
