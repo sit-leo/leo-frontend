2a9a787eece0166272a49bd931eda5df071559a6
Keerati <keerati.jear@gmail.com>
2019-11-14 01:56:35 +0700

    [ADD] add my organization and signup page to storybook

--- .storybook/stories/organization/index.js
+++ .storybook/stories/organization/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import MyOrganizationsPage from '../../../components/organization/MyOrganizationsPage';
 import DashboardPage from '../../../components/organization/DashboardPage';
 import MatchManagementPage from '../../../components/organization/MatchManagementPage';
 import AddRecruiterPage from '../../../components/organization/AddRecruiterPage';
 import AddApplicantPage from '../../../components/organization/AddApplicantPage';
 
 storiesOf('Organization', module)
+  .add('My Organization Component', () => (
+    <MyOrganizationsPage />
+  ))
   .add('Dashboard Component', () => (
     <DashboardPage />
   ))
--- .storybook/stories/user/index.js
+++ .storybook/stories/user/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import LoginPage from '../../../components/user/LoginPage';
+import SignupPage from '../../../components/user/SignupPage';
 
 storiesOf('User', module)
   .add('Login Component', () => (
     <LoginPage />
+  ))
+  .add('Signup Component', () => (
+    <SignupPage />
   ))
