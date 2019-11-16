fdc57d06ced1931a72247a64c5d21eb4fcf7c872
Keerati <keerati.jear@gmail.com>
2019-11-17 03:23:46 +0700

    [UPDATE] implement example chart and ready for data

--- components/organization/Chart.js
+++ components/organization/Chart.js
@@ -1,30 +1,64 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 
+import color from '../../config/color';
+
+const CHART_COLORS = {
+  matches: '#3794fc',
+  applicantParticipant: '#9d60fb',
+  recruiterParticipant: '#fb6081',
+  unMatchedApplicants: '#fbd860',
+  unMatchedRecruiters: '#fb8c60',
+};
+
+function generateNumbers() {
+  const numbers = [];
+  while (numbers.length < 12) {
+    const number = Number.parseInt((Math.random() * 500), 10);
+    if (number < 500 && number > 480) {
+      numbers.push(number);
+    }
+  }
+  return numbers;
+}
+
+function createDataset(label, data, borderColor) {
+  return {
+    label,
+    borderColor,
+    fill: false,
+    lineTension: 0,
+    backgroundColor: borderColor,
+    pointBackgroundColor: color.white,
+    pointBorderColor: borderColor,
+    pointBorderWidth: '2',
+    borderWidth: '2',
+    pointHoverBackgroundColor: color.white,
+    data,
+  };
+}
+
 const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  labels: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
   datasets: [
-    {
-      label: 'Matching Statistics',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40],
-    },
+    createDataset('Matches', generateNumbers(), CHART_COLORS.matches),
+    createDataset('Applicants Participanting', generateNumbers(), CHART_COLORS.applicantParticipant),
+    createDataset('Recruiters Participanting', generateNumbers(), CHART_COLORS.recruiterParticipant),
+    createDataset('Unmatched Applicants', generateNumbers(), CHART_COLORS.unMatchedApplicants),
+    createDataset('Unmatched Recruiters', generateNumbers(), CHART_COLORS.unMatchedRecruiters),
   ],
 };
 
